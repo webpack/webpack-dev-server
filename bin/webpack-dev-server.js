99c6d9937e4b7d964b4c020641e7bc3f89b5c0a4
Mark Wolfe <mark@wolfe.id.au>
2015-10-04 17:57:51 +1100

    Added a flag for public ip to seed the inline frame.
    Note: As this will default to using host to ensure backward
    compatibility.

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -46,6 +46,8 @@ var optimist = require("optimist")
 
 	.describe("port", "The port").default("port", 8080)
 
+	.describe("public", "The public hostname/ip address of the server")
+
 	.describe("host", "The hostname/ip address the server will bind to").default("host", "localhost");
 
 require("webpack/bin/config-optimist")(optimist);
@@ -59,6 +61,9 @@ var options = wpOpt.devServer || firstWpOpt.devServer || {};
 
 if(argv.host !== "localhost" || !options.host)
 	options.host = argv.host;
+
+if(!options.public)
+	options.public = argv.public;
 
 if(argv.port !== 8080 || !options.port)
 	options.port = argv.port;
@@ -140,7 +145,7 @@ if(argv["compress"])
 var protocol = options.https ? "https" : "http";
 
 if(options.inline) {
-	var devClient = [require.resolve("../client/") + "?" + protocol + "://" + options.host + ":" + options.port];
+	var devClient = [require.resolve("../client/") + "?" + protocol + "://" + (options.public || (options.host + ":" + options.port))];
 
 	if(options.hot)
 		devClient.push("webpack/hot/dev-server");
