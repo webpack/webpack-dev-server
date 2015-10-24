67ff2d53c6d16cc07dbb50755ae9e4fe2e46e23f
Kees Kluskens <kees@webduck.nl>
2015-10-24 20:10:06 +0200

    Add socket handlers.

--- client/index.js
+++ client/index.js
@@ -13,6 +13,47 @@ var hot = false;
 var initial = true;
 var currentHash = "";
 
+var onSocketMsg = {
+	hot: function() {
+		hot = true;
+		console.log("[WDS] Hot Module Replacement enabled.");
+	},
+	invalid: function() {
+		console.log("[WDS] App updated. Recompiling...");
+	},
+	hash: function(hash) {
+		currentHash = hash;
+	},
+	"still-ok": function() {
+		console.log("[WDS] Nothing changed.")
+	},
+	ok: function() {
+		if(initial) return initial = false;
+		reloadApp();
+	},
+	warnings: function(warnings) {
+		console.log("[WDS] Warnings while compiling.");
+		for(var i = 0; i < warnings.length; i++)
+			console.warn(stripAnsi(warnings[i]));
+		if(initial) return initial = false;
+		reloadApp();
+	},
+	errors: function(errors) {
+		console.log("[WDS] Errors while compiling.");
+		for(var i = 0; i < errors.length; i++)
+			console.error(stripAnsi(errors[i]));
+		if(initial) return initial = false;
+		reloadApp();
+	},
+	"proxy-error": function(errors) {
+		console.log("[WDS] Proxy error.");
+		for(var i = 0; i < errors.length; i++)
+			console.error(stripAnsi(errors[i]));
+		if(initial) return initial = false;
+		reloadApp();
+	}
+};
+
 var newConnection = function() {
 	sock = new SockJS(url.format({
 		protocol: urlParts.protocol,
