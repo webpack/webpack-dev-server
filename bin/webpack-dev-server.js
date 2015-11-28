098981036caec7f6b3a4907582b64bf9409c9c39
Kees Kluskens <kees@webduck.nl>
2015-11-28 10:39:53 +0100

    Open default browser if --open option is given

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -2,6 +2,7 @@
 
 var path = require("path");
 var url = require("url");
+var open = require("open");
 var fs = require("fs");
 
 // Local version replaces global one
@@ -44,7 +45,10 @@ var optimist = require("optimist")
 
 	.boolean("compress").describe("compress", "enable gzip compression")
 
+	.boolean("open").describe("open", "Open default browser")
+
 	.describe("port", "The port").default("port", 8080)
+
 
 	.describe("host", "The hostname/ip address the server will bind to").default("host", "localhost");
 
@@ -137,6 +141,9 @@ if(argv["history-api-fallback"])
 if(argv["compress"])
 	options.compress = true;
 
+if(argv["open"])
+	options.open = true;
+
 var protocol = options.https ? "https" : "http";
 
 if(options.inline) {
@@ -156,11 +163,13 @@ if(options.inline) {
 }
 
 new Server(webpack(wpOpt), options).listen(options.port, options.host, function(err) {
+	var uri = protocol + "://" + options.host + ":" + options.port + "/";
+
 	if(err) throw err;
 	if(options.inline)
-		console.log(protocol + "://" + options.host + ":" + options.port + "/");
+		console.log(uri);
 	else
-		console.log(protocol + "://" + options.host + ":" + options.port + "/webpack-dev-server/");
+		console.log(uri + "webpack-dev-server/");
 	console.log("webpack result is served from " + options.publicPath);
 	if(typeof options.contentBase === "object")
 		console.log("requests are proxied to " + options.contentBase.target);
@@ -168,4 +177,6 @@ new Server(webpack(wpOpt), options).listen(options.port, options.host, function(
 		console.log("content is served from " + options.contentBase);
 	if(options.historyApiFallback)
 		console.log("404s will fallback to %s", options.historyApiFallback.index || "/index.html");
+	if (options.open)
+		open(uri);
 });
