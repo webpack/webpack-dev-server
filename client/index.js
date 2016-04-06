924bd1b20110f99d427fa3d62d286a4514e5997b
svsool <svsool@bk.ru>
2016-04-06 22:04:56 +0300

    fix bug when src attribute not defined on script element

--- client/index.js
+++ client/index.js
@@ -8,7 +8,8 @@ if (typeof __resourceQuery === "string" && __resourceQuery) {
 } else {
 	// Else, get the url from the <script> this file was called with.
 	var scriptElements = document.getElementsByTagName("script");
-	var scriptHost = scriptElements[scriptElements.length-1].getAttribute("src").replace(/\/[^\/]+$/, "");
+	var scriptHost = scriptElements[scriptElements.length-1].getAttribute("src");
+	scriptHost = scriptHost && scriptHost.replace(/\/[^\/]+$/, "");
 	urlParts = url.parse((scriptHost ? scriptHost : "/"), false, true);
 }
 
