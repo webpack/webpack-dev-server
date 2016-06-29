webpack/webpack-dev-server

commit 3d3c000df8889e8b557995e5fe918b29072c377f

Fix socket connection for non-inlined connections

client/index.js
@@ -2,10 +2,13 @@ var url = require('url');
 var SockJS = require("sockjs-client");
 var stripAnsi = require('strip-ansi');
 var scriptElements = document.getElementsByTagName("script");
+var scriptHost = scriptElements[scriptElements.length-1].getAttribute("src").replace(/\/[^\/]+$/, "");
 
+// If this bundle is inlined, use the resource query to get the correct url.
+// Else, get the url from the <script> this file was called with.
 var urlParts = url.parse(typeof __resourceQuery === "string" && __resourceQuery ?
 	__resourceQuery.substr(1) :
-	scriptElements[scriptElements.length-1].getAttribute("src").replace(/\/[^\/]+$/, "")
+	(scriptHost ? scriptHost : "/")
 );
 
 var sock = null;

commit 22712286a5656708d73bca49e9dd2b08184ad48a

Handle missing `src` attribute on unused last script
This fixes a bug introduced in 3d3c000, in the case where:

- `__resourceQuery` is set, and
- The last script tag on the page has no `src` attribute

Previously the script tag's `src` attribute was accessed lazily only if
`__resourceQuery` was not defined.  The last script on the page may not be the
script that loaded webpack-dev-server.  This can happen, for example, with the
use of an asynchronous loader such as LABjs.

client/index.js
@@ -2,7 +2,8 @@ var url = require('url');
 var SockJS = require("sockjs-client");
 var stripAnsi = require('strip-ansi');
 var scriptElements = document.getElementsByTagName("script");
-var scriptHost = scriptElements[scriptElements.length-1].getAttribute("src").replace(/\/[^\/]+$/, "");
+var scriptSrc = scriptElements[scriptElements.length-1].getAttribute("src");
+var scriptHost = scriptSrc && scriptSrc.replace(/\/[^\/]+$/, "");
 
 // If this bundle is inlined, use the resource query to get the correct url.
 // Else, get the url from the <script> this file was called with.