92e93e6f5e167cee89256f400dd8686ed513516f
Stevi Deter <stevi.deter@providence.org>
2016-09-13 09:47:14 -0700

    backport fix for https and add null check

--- client/index.js
+++ client/index.js
@@ -2,14 +2,30 @@ var url = require('url');
 var SockJS = require("sockjs-client");
 var stripAnsi = require('strip-ansi');
 
+function getCurrentScriptSource() {
+	// `document.currentScript` is the most accurate way to find the current script,
+	// but is not supported in all browsers.
+	if(document.currentScript)
+		return document.currentScript.getAttribute("src");
+	// Fall back to getting all scripts in the document.
+	var scriptElements = document.scripts || [];
+	var currentScript = scriptElements[scriptElements.length - 1];
+	if(currentScript)
+		return currentScript.getAttribute("src");
+	// Fail as there was no script to use.
+	throw new Error("[WDS] Failed to get current script source");
+}
+
 // If this bundle is inlined, use the resource query to get the correct url.
 // Else, get the url from the <script> this file was called with.
 var urlParts;
 if (typeof __resourceQuery === "string" && __resourceQuery) {
 	urlParts = url.parse(__resourceQuery.substr(1));
 } else {
-	var scriptElements = document.getElementsByTagName("script");
-	urlParts = url.parse(scriptElements[scriptElements.length-1].getAttribute("src").replace(/\/[^\/]+$/, ""))
+	// Else, get the url from the <script> this file was called with.
+	var scriptHost = getCurrentScriptSource();
+	scriptHost = scriptHost.replace(/\/[^\/]+$/, "");
+	urlParts = url.parse((scriptHost ? scriptHost : "/"), false, true);
 }
 
 var sock = null;
@@ -57,14 +73,29 @@ var onSocketMsg = {
 	}
 };
 
-var newConnection = function() {
-	sock = new SockJS(url.format({
+
+var hostname = urlParts.hostname;
+if(!urlParts.hostname || urlParts.hostname === '0.0.0.0') {
+	// why do we need this check?
+	// hostname n/a for file protocol (example, when using electron, ionic)
+	// see: https://github.com/webpack/webpack-dev-server/pull/384
+	if(window.location.hostname && !!~window.location.protocol.indexOf('http')) {
+		hostname = window.location.hostname;
+	}
+}
+
+var port = (!urlParts.port || urlParts.port === '0') ? window.location.port : urlParts.port;
+
+var formattedUrl = url.format({
 		protocol: (window.location.protocol === "https:" || urlParts.hostname === '0.0.0.0') ? window.location.protocol : urlParts.protocol,
 		auth: urlParts.auth,
-		hostname: (urlParts.hostname === '0.0.0.0') ? window.location.hostname : urlParts.hostname,
-		port: (urlParts.port === '0') ? window.location.port : urlParts.port,
+		hostname: hostname,
+		port: port,
 		pathname: urlParts.path == null || urlParts.path === '/' ? "/sockjs-node" : urlParts.path
-	}));
+	});
+
+var newConnection = function() {
+	sock = new SockJS(formattedUrl);
 
 	sock.onclose = function() {
 		console.error("[WDS] Disconnected!");
