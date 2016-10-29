webpack/webpack-dev-server

commit 272c78a74e2f9414d9ef41b56acec70bb19f4b5f

Fixed ipv6 [::] (#676)
* fixed ipv6 parsing and listening [::] with latest
* add example

bin/webpack-dev-server.js
@@ -281,8 +281,15 @@ function processOptions(wpOpt) {
 
 	var protocol = options.https ? "https" : "http";
 
+	// the formated domain (url without path) of the webpack server
+	var domain = url.format({
+		protocol: protocol,
+		hostname: options.host,
+		port: options.socket ? 0 : options.port.toString()
+	});
+
 	if(options.inline !== false) {
-		var devClient = [require.resolve("../client/") + "?" + protocol + "://" + (options.public || (options.host + ":" + options.port))];
+		var devClient = [require.resolve("../client/") + "?" + (options.public ? protocol + "://" + options.public : domain)];
 
 		if(options.hotOnly)
 			devClient.push("webpack/hot/only-dev-server");
@@ -308,12 +315,7 @@ function processOptions(wpOpt) {
 		}));
 	}
 
-	var uri = url.format({
-		protocol: protocol,
-		hostname: options.host,
-		pathname: options.inline !== false ? "/" : "webpack-dev-server/",
-		port: options.socket ? 0 : options.port.toString()
-	});
+	var uri = domain + (options.inline !== false ? "/" : "webpack-dev-server/");
 
 	var server = new Server(compiler, options);
 

client/index.js
@@ -89,7 +89,9 @@ var onSocketMsg = {
 var hostname = urlParts.hostname;
 var protocol = urlParts.protocol;
 
-if(urlParts.hostname === "0.0.0.0") {
+
+//check ipv4 and ipv6 `all hostname`
+if(hostname === "0.0.0.0" || hostname === "::") {
 	// why do we need this check?
 	// hostname n/a for file protocol (example, when using electron, ionic)
 	// see: https://github.com/webpack/webpack-dev-server/pull/384

commit 36817dc175303ceb9cb86b0f5c47b573b5584a05

Fix incorrect URL when not using inline modus (introduced in #676)

bin/webpack-dev-server.js
@@ -315,7 +315,7 @@ function processOptions(wpOpt) {
 		}));
 	}
 
-	var uri = domain + (options.inline !== false ? "/" : "webpack-dev-server/");
+	var uri = domain + (options.inline !== false ? "/" : "/webpack-dev-server/");
 
 	var server = new Server(compiler, options);
 