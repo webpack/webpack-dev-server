00996138ba2a283401184d13c73048e7216df706
Alistair Brown <github@alistairjcbrown.com>
2017-02-06 10:34:35 +0000

    Handle promise rejection when using `--open` flag (#780)

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -434,8 +434,11 @@ function reportReadiness(uri, options) {
 		console.log(`Content not from webpack is served from ${colorInfo(useColor, contentBase)}`);
 	if(options.historyApiFallback)
 		console.log(`404s will fallback to ${colorInfo(useColor, options.historyApiFallback.index || "/index.html")}`);
-	if(options.open)
-		open(uri);
+	if(options.open) {
+		open(uri).catch(function() {
+			console.log("Unable to open browser. If you are running in a headless environment, please do not use the open flag.");
+		});
+	}
 }
 
 processOptions(wpOpt);
