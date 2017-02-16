3d8ccd5eee29bda194fba8c46aff4748d94fd818
Juho Vepsalainen <bebraw@gmail.com>
2017-02-16 09:35:00 +0200

    feat - Allow overlay to be enabled for warnings
    Now you can use
    
    {
      overlay: {
        errors: true,
        warnings: true
      }
    }
    
    Closes #789.

---
+++ examples/overlay-warnings/app.js
@@ -0,0 +1,8 @@
+document.write("It's working.");
+
+// This results in a warning:
+// require("./casesensitive.js");
+// require("./caseSensitive.js");
+
+// This results in an error:
+// if(!window) require("test");
