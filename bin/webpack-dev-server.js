b5f064e9b4411b93bbd56632ae1df2690981d9ae
Frank Winter <f.winter@alice-dsl.net>
2017-03-03 21:57:14 +0100

    Fix code style issues

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -307,7 +307,7 @@ function processOptions(wpOpt) {
 		options.compress = true;
 
 	if(argv["open"])
-	  options.open = (argv["open"] === '') ? true : argv["open"];
+		options.open = (argv["open"] === "") ? true : argv["open"];
 
 	// Kind of weird, but ensures prior behavior isn't broken in cases
 	// that wouldn't throw errors. E.g. both argv.port and options.port
@@ -410,14 +410,14 @@ function reportReadiness(uri, options) {
 	if(options.historyApiFallback)
 		console.log(`404s will fallback to ${colorInfo(useColor, options.historyApiFallback.index || "/index.html")}`);
 	if(options.open) {
-	  var params = [uri];
+		var params = [uri];
 
-    if (typeof options.open === 'string')
-      params.push({ app: options.open });
+		if (typeof options.open === "string")
+			params.push({ app: options.open });
 
-    open(...params).catch(function() {
-      console.log("Unable to open browser. If you are running in a headless environment, please do not use the open flag.");
-    });
+		open(...params).catch(function() {
+			console.log("Unable to open browser. If you are running in a headless environment, please do not use the open flag.");
+		});
 	}
 }
 
