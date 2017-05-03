d085a1f7215015c59ccef99d34ea17ee456d5032
sliwey <qlw1009@gmail.com>
2017-05-03 16:52:39 +0800

    Add 'lan' option
      Let the browser open with local IP.

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -89,6 +89,10 @@ yargs.options({
 		type: "boolean",
 		describe: "Open default browser"
 	},
+	"lan": {
+		type: "boolean",
+		describe: "Open default browser with local IP"
+	},
 	"color": {
 		type: "boolean",
 		alias: "colors",
@@ -312,6 +316,9 @@ function processOptions(wpOpt) {
 
 	if(argv["open"])
 		options.open = true;
+
+	if(argv["lan"])
+		options.lan = true;
 
 	// Kind of weird, but ensures prior behavior isn't broken in cases
 	// that wouldn't throw errors. E.g. both argv.port and options.port
