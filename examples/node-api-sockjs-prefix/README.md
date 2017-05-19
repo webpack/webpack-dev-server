0ab4e3c28c006daff70de5ad81ded43076e6c314
kellyrmilligan <kelly.milligan@gmail.com>
2017-05-19 09:54:41 -0500

    updates to example for sockjsprefix

---
+++ examples/node-api-sockjs-prefix/README.md
@@ -0,0 +1,15 @@
+# Node.js API - Simple
+
+```shell
+node server.js
+```
+
+Starts a simple webpack-dev-server setup via the Node API. Open `http://localhost:8080/` to go the app.
+
+## What should happen
+
+In the app you should see "It's working."
+
+In `app.js`, uncomment the code that results in an error and save. This error should be visible in the CLI and devtools.
+
+Then, in `app.js`, uncomment the code that results in a warning. This warning should be visible in the CLI and devtools.
--- examples/node-api-sockjs-prefix/server.js
+++ examples/node-api-sockjs-prefix/server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Webpack = require("webpack");
+const path = require("path");
 const WebpackDevServer = require("../../lib/Server");
 const webpackConfig = require("./webpack.config");
 
@@ -8,7 +9,15 @@ const compiler = Webpack(webpackConfig);
 const server = new WebpackDevServer(compiler, {
 	stats: {
 		colors: true
-	}
+	},
+	contentBase: path.resolve(__dirname),
+	watchContentBase: true,
+	sockjsPrefix: "/subapp",
+	publicPath: "/subapp/",
+	historyApiFallback: {
+		disableDotRule: true,
+		index: "/subapp/"
+	},
 });
 
 server.listen(8080, "127.0.0.1", function() {
