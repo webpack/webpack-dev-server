41ceb4b7ae871a465e6cd1453f530743d1563595
shellscape <andrew@shellscape.org>
2017-08-08 14:06:36 -0400

    Revert "Sockjs prefix config (#911)"
    This reverts commit ccd113a36e4217ef30e8f9679b4ab58773795b38.

--- examples/node-api-sockjs-prefix/app.js
+++
@@ -1,7 +0,0 @@
-document.write("It's working under a subapp");
-
-// This results in a warning:
-if(!window) require("./" + window + "parseable.js");
-
-// This results in an error:
-// if(!window) require("test");
--- examples/node-api-sockjs-prefix/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	context: __dirname,
-	entry: ["./app.js", "../../client/index.js?http://localhost:8080/"],
-	output: {
-		filename: "bundle.js",
-		publicPath: "/subapp/"
-	},
-}
