facb8eab03366239abece9dfb382fddfe3e27af7
Zane Hitchcox <zwhitchcox@gmail.com>
2017-09-02 14:29:22 -0400

    Update webpack.config.js

--- examples/node-api-simple/webpack.config.js
+++ examples/node-api-simple/webpack.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   context: __dirname,
-  entry: ['../../client/index.js?http://localhost:8080/', './app.js', ],
+  entry: ['../../client/index.js?http://localhost:8080/', './app.js'],
   output: {
     filename: 'bundle.js'
   }
