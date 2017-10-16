4a7cb6cc90baae5716dd75f9e35743e90b66b058
shellscape <andrew@shellscape.org>
2017-10-16 18:04:15 -0400

    fixes promise webpack configs

--- cli.js
+++ cli.js
@@ -14,22 +14,24 @@ const options = require('./lib/cli/options');
 const start = require('./lib/cli/start');
 const pkg = require('./package.json');
 
-const localCLI = resolveCwd.silent('webpack-dev-server/cli');
+const localCli = resolveCwd.silent('webpack-dev-server/cli');
 
 // Prefer the local installation of webpack-dev-server
-if (localCLI && localCLI !== __filename) {
+if (localCli && localCli !== __filename) {
   debug('Using local install of webpack-dev-server');
   // eslint-disable-next-line
-  require(localCLI);
+  require(localCli);
   return;
 }
 
 updateNotifier({ pkg }).notify();
 
-const webpackOptions = convertArgv(yargs, argv, {
+const webpackOpts = convertArgv(yargs, argv, {
   outputFilename: '/bundle.js'
 });
 
-options(argv, webpackOptions).then((devServerOptions) => {
+options(argv, webpackOpts).then((result) => {
+  const { devServerOptions, webpackOptions } = result;
+
   start(argv, devServerOptions, webpackOptions);
 });
