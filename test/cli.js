7284c51643beff254e85a0f155f6092da7a60c6c
shellscape <andrew@shellscape.org>
2017-11-27 09:28:41 -0500

    reimplementing SIGINT improvements from master

--- test/cli.js
+++ test/cli.js
@@ -2,11 +2,11 @@
 
 const assert = require('assert');
 const path = require('path');
-const execa = require('execa');
+const execa = require('execa'); // eslint-disable-line import/no-extraneous-dependencies
 
 describe('SIGINT', () => {
   it('should exit the process when SIGINT is detected', (done) => {
-    const cliPath = path.resolve(__dirname, '../bin/webpack-dev-server.js');
+    const cliPath = path.resolve(__dirname, '../cli.js');
     const examplePath = path.resolve(__dirname, '../examples/cli/public');
     const nodePath = execa.shellSync('which node').stdout;
 
@@ -21,7 +21,14 @@ describe('SIGINT', () => {
       }
     });
 
-    proc.on('exit', () => {
+    process.on('unhandledRejection', (reason, p) => {
+      throw new Error(`Unhandled Rejection at: ${p}, 'reason: ${reason}`);
+    });
+
+    proc.on('exit', (code) => {
+      if (code !== 0) {
+        throw new Error(`Process exited with code: ${code}`);
+      }
       done();
     });
   }).timeout(4000);
