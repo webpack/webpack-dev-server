345862779133fc45eb949cb6316f948c11949a18
mehyaa <mehyaa@gmail.com>
2018-01-16 12:18:30 +0300

    openArgs added to validation test

--- test/Validation.test.js
+++ test/Validation.test.js
@@ -50,7 +50,7 @@ describe('Validation', () => {
       " - configuration has an unknown property 'asdf'. These properties are valid:",
       '   object { hot?, hotOnly?, lazy?, bonjour?, host?, allowedHosts?, filename?, publicPath?, port?, socket?, ' +
       'watchOptions?, headers?, clientLogLevel?, overlay?, progress?, key?, cert?, ca?, pfx?, pfxPassphrase?, requestCert?, ' +
-      'inline?, disableHostCheck?, public?, https?, contentBase?, watchContentBase?, open?, useLocalIp?, openPage?, features?, ' +
+      'inline?, disableHostCheck?, public?, https?, contentBase?, watchContentBase?, open?, useLocalIp?, openPage?, openArgs?, features?, ' +
       'compress?, proxy?, historyApiFallback?, staticOptions?, setup?, before?, after?, stats?, reporter?, reportTime?, ' +
       'noInfo?, quiet?, serverSideRender?, index?, log?, warn? }'
     ]
