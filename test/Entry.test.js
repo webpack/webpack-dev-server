f4f14cec31f25ffb46898d1e281fe9569a8569ae
Mike Greiling <mike@pixelcog.com>
2018-02-25 03:57:37 -0600

    Fix support for DynamicEntryPlugin (#1319)
    * Fix incorrect use of assert library and unnecessary done callback
    
    * Add tests for each type of entry option
    
    * Add proper support for webpack's DynamicEntryPlugin

--- test/Entry.test.js
+++ test/Entry.test.js
@@ -2,17 +2,112 @@
 
 const assert = require('assert');
 const addDevServerEntrypoints = require('../lib/util/addDevServerEntrypoints');
+const config = require('./fixtures/simple-config/webpack.config');
 
 describe('Entry', () => {
-  it('default to src if no entry point is given', (done) => {
+  it('adds devServer entry points to a single entry point', () => {
+    const webpackOptions = Object.assign({}, config);
+    const devServerOptions = {};
+
+    addDevServerEntrypoints(webpackOptions, devServerOptions);
+
+    assert.equal(webpackOptions.entry.length, 2);
+    assert(webpackOptions.entry[0].indexOf('client/index.js?') !== -1);
+    assert.equal(webpackOptions.entry[1], './foo.js');
+  });
+
+  it('adds devServer entry points to a multi-module entry point', () => {
+    const webpackOptions = Object.assign({}, config, {
+      entry: ['./foo.js', './bar.js']
+    });
+    const devServerOptions = {};
+
+    addDevServerEntrypoints(webpackOptions, devServerOptions);
+
+    assert.equal(webpackOptions.entry.length, 3);
+    assert(webpackOptions.entry[0].indexOf('client/index.js?') !== -1);
+    assert.equal(webpackOptions.entry[1], './foo.js');
+    assert.equal(webpackOptions.entry[2], './bar.js');
+  });
+
+  it('adds devServer entry points to a multi entry point object', () => {
+    const webpackOptions = Object.assign({}, config, {
+      entry: {
+        foo: './foo.js',
+        bar: './bar.js'
+      }
+    });
+    const devServerOptions = {};
+
+    addDevServerEntrypoints(webpackOptions, devServerOptions);
+
+    assert.equal(webpackOptions.entry.foo.length, 2);
+    assert(webpackOptions.entry.foo[0].indexOf('client/index.js?') !== -1);
+    assert.equal(webpackOptions.entry.foo[1], './foo.js');
+    assert.equal(webpackOptions.entry.bar[1], './bar.js');
+  });
+
+  it('defaults to src if no entry point is given', () => {
     const webpackOptions = {};
     const devServerOptions = {};
 
     addDevServerEntrypoints(webpackOptions, devServerOptions);
 
-    assert(webpackOptions.entry.length, 2);
-    assert(webpackOptions.entry[1], './src');
+    assert.equal(webpackOptions.entry.length, 2);
+    assert.equal(webpackOptions.entry[1], './src');
+  });
 
-    done();
+  it('preserves dynamic entry points', (done) => {
+    let i = 0;
+    const webpackOptions = {
+      // simulate dynamic entry
+      entry: () => {
+        i += 1;
+        return `./src-${i}.js`;
+      }
+    };
+    const devServerOptions = {};
+
+    addDevServerEntrypoints(webpackOptions, devServerOptions);
+
+    assert(typeof webpackOptions.entry, 'function');
+
+    webpackOptions.entry().then(entryFirstRun => (
+      webpackOptions.entry().then((entrySecondRun) => {
+        assert.equal(entryFirstRun.length, 2);
+        assert.equal(entryFirstRun[1], './src-1.js');
+
+        assert.equal(entrySecondRun.length, 2);
+        assert.equal(entrySecondRun[1], './src-2.js');
+        done();
+      })
+    )).catch(done);
+  });
+
+  it('preserves asynchronous dynamic entry points', (done) => {
+    let i = 0;
+    const webpackOptions = {
+      // simulate async dynamic entry
+      entry: () => new Promise((resolve) => {
+        i += 1;
+        resolve(`./src-${i}.js`);
+      })
+    };
+    const devServerOptions = {};
+
+    addDevServerEntrypoints(webpackOptions, devServerOptions);
+
+    assert(typeof webpackOptions.entry, 'function');
+
+    webpackOptions.entry().then(entryFirstRun => (
+      webpackOptions.entry().then((entrySecondRun) => {
+        assert.equal(entryFirstRun.length, 2);
+        assert.equal(entryFirstRun[1], './src-1.js');
+
+        assert.equal(entrySecondRun.length, 2);
+        assert.equal(entrySecondRun[1], './src-2.js');
+        done();
+      })
+    )).catch(done);
   });
 });
