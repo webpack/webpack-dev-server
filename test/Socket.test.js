b2ef4dea1f9f65bffb5f0b325cb2558d9c6b9bfb
Thomas Crescenzi <trescenzi@gmail.com>
2018-10-31 16:13:04 -0400

    add real tests for sockPath

--- test/Socket.test.js
+++ test/Socket.test.js
@@ -1,35 +1,53 @@
 'use strict';
 
 const assert = require('assert');
+const path = require('path');
+const request = require('request');
 const config = require('./fixtures/simple-config/webpack.config');
 const helper = require('./helper');
 
+const requestSucceeds = (done) => (err, res, body) => {
+  if (err) {
+    done(err);
+  }
+
+  assert.equal(res.statusCode, 200);
+  done();
+}
 
 describe('socket options', () => {
   let server;
+  let req;
 
   afterEach(helper.close);
-
   describe('default behavior', () => {
-    before((done) => {
+    beforeEach((done) => {
       server = helper.start(config, {}, done);
     });
 
     it('defaults to a path', () => {
       assert.ok(server.sockPath.match(/\/[a-z0-9\-/]+[^/]$/));
     });
+
+    it('responds with a 200', (done) => {
+      request(`http://localhost:8080/sockjs-node`, requestSucceeds(done));
+    });
   });
 
   describe('socksPath option', () => {
     const path = '/foo/test/bar';
-    before((done) => {
+    beforeEach((done) => {
       server = helper.start(config, {
         sockPath: '/foo/test/bar/'
       }, done);
     });
 
-    it('correctly sets the servers sockPath including removal of extraneous slashes', () => {
-      assert.equal(path, server.sockPath);
+    it('sets the sock path correctly and strips leading and trailing /s', () => {
+      assert.equal(server.sockPath, path);
     });
+
+    it('responds with a 200 second', (done) => {
+      request(`http://localhost:8080${path}`, requestSucceeds(done));
+    })
   });
 });
