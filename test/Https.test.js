0b89fd94b78f6e049614ab466a7eab7d6a2a33f2
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-01-30 22:08:37 +0300

    feat: allow to use `ca`, `pfx`, `key` and `cert` as string (#1542)

--- test/Https.test.js
+++ test/Https.test.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const request = require('supertest');
 const helper = require('./helper');
 const config = require('./fixtures/contentbase-config/webpack.config');
 require('mocha-sinon');
 
+const httpsCertificateDirectory = path.join(__dirname, 'fixtures/https-certificate');
 const contentBasePublic = path.join(__dirname, 'fixtures/contentbase-config/public');
 
 describe('HTTPS', function testHttps() {
@@ -21,6 +23,48 @@ describe('HTTPS', function testHttps() {
       server = helper.start(config, {
         contentBase: contentBasePublic,
         https: true
+      }, done);
+      req = request(server.app);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/')
+        .expect(200, /Heyo/, done);
+    });
+  });
+
+  describe('ca, pfx, key and cert are buffer', () => {
+    before((done) => {
+      server = helper.start(config, {
+        contentBase: contentBasePublic,
+        https: {
+          ca: fs.readFileSync(path.join(httpsCertificateDirectory, 'ca.pem')),
+          pfx: fs.readFileSync(path.join(httpsCertificateDirectory, 'server.pfx')),
+          key: fs.readFileSync(path.join(httpsCertificateDirectory, 'server.key')),
+          cert: fs.readFileSync(path.join(httpsCertificateDirectory, 'server.crt')),
+          passphrase: 'webpack-dev-server'
+        }
+      }, done);
+      req = request(server.app);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/')
+        .expect(200, /Heyo/, done);
+    });
+  });
+
+  describe('ca, pfx, key and cert are string', () => {
+    before((done) => {
+      server = helper.start(config, {
+        contentBase: contentBasePublic,
+        https: {
+          ca: path.join(httpsCertificateDirectory, 'ca.pem'),
+          pfx: path.join(httpsCertificateDirectory, 'server.pfx'),
+          key: path.join(httpsCertificateDirectory, 'server.key'),
+          cert: path.join(httpsCertificateDirectory, 'server.crt'),
+          passphrase: 'webpack-dev-server'
+        }
       }, done);
       req = request(server.app);
     });
