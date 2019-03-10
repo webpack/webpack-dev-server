c780289aaa3305961f6c51ef9b5d4dee3594fec5
daniel <daniel.mohns@posteo.de>
2019-03-10 20:50:35 +0100

    Add test for mimeTypes option

--- test/MimeTypes.test.js
+++ test/MimeTypes.test.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const request = require('supertest');
 const helper = require('./helper');
 const config = require('./fixtures/simple-config/webpack.config');
 
-describe('MimeTypes', () => {
+describe('MimeTypes configuration', () => {
   afterEach(helper.close);
 
   it('remapping mime type without force should throw an error', () => {
     expect(() => {
       helper.start(config, {
-        mimeTypes: { 'text/html': ['js'] },
+        mimeTypes: { 'application/octet-stream': ['js'] },
       });
     }).toThrow(/Attempt to change mapping for/);
   });
@@ -19,11 +20,39 @@ describe('MimeTypes', () => {
       config,
       {
         mimeTypes: {
-          typeMap: { 'text/html': ['js'] },
+          typeMap: { 'application/octet-stream': ['js'] },
           force: true,
         },
       },
       done
     );
+  });
+});
+
+describe('custom mimeTypes', () => {
+  let server;
+  let req;
+
+  beforeAll((done) => {
+    server = helper.start(
+      config,
+      {
+        mimeTypes: {
+          typeMap: { 'application/octet-stream': ['js'] },
+          force: true,
+        },
+      },
+      done
+    );
+    req = request(server.app);
+  });
+
+  afterAll(helper.close);
+
+  it('request to bundle file with modified mime type', (done) => {
+    req
+      .get('/main.js')
+      .expect('Content-Type', /application\/octet-stream/)
+      .expect(200, done);
   });
 });
