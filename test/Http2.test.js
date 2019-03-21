9167003ef9b45f181f208bdab4332858c8585e73
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-03-21 01:09:33 -0500

    deprecation warning to not use https.spdy option, adjust http2 test to run only within certain versions

--- test/Http2.test.js
+++ test/Http2.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const path = require('path');
-const http2 = require('http2');
 const request = require('supertest');
 const semver = require('semver');
 const helper = require('./helper');
@@ -12,23 +11,24 @@ const contentBasePublic = path.join(
   'fixtures/contentbase-config/public'
 );
 
-describe('HTTP2', () => {
+describe('http2', () => {
   let server;
   let req;
 
-  describe('without https it should throw an error', () => {
-    it('create server', () => {
-      expect(() => {
-        helper.start(config, {
-          http2: true,
-        });
-      }).toThrow(/'https' option must be enabled/);
-    });
-  });
-
   // HTTP/2 will only work with node versions below 10.0.0
-  // since spdy is broken past that point
-  if (semver.lt(process.version, '10.0.0')) {
+  // since spdy is broken past that point, and this test will only
+  // work above Node 8.8.0, since it is the first version where the
+  // built-in http2 module is exposed without need for a flag
+  // (https://nodejs.org/en/blog/release/v8.8.0/)
+  // if someone is testing below this Node version and breaks this,
+  // their tests will not catch it, but CI will catch it.
+  if (
+    semver.gte(process.version, '8.8.0') &&
+    semver.lt(process.version, '10.0.0')
+  ) {
+    /* eslint-disable global-require */
+    const http2 = require('http2');
+    /* eslint-enable global-require */
     describe('http2 works with https', () => {
       beforeAll((done) => {
         server = helper.start(
@@ -70,6 +70,26 @@ describe('HTTP2', () => {
       afterAll(helper.close);
     });
   }
+
+  describe('server works with http2 option, but without https option', () => {
+    beforeAll((done) => {
+      server = helper.start(
+        config,
+        {
+          contentBase: contentBasePublic,
+          http2: true,
+        },
+        done
+      );
+      req = request(server.app);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/').expect(200, /Heyo/, done);
+    });
+
+    afterAll(helper.close);
+  });
 
   describe('https without http2 disables HTTP/2', () => {
     beforeAll((done) => {
