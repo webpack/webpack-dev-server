2babccf34a36cf5ffc96c51cd1809d9995abcd30
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-03-23 15:27:08 -0500

    make sure that page.goto is done before closing browser and proceeding to next puppeteer test

--- test/Hot.test.js
+++ test/Hot.test.js
@@ -5,6 +5,17 @@ const config = require('./fixtures/client-config/webpack.config');
 const multiCompilerConfig = require('./fixtures/multi-compiler-config/webpack.config');
 const runBrowser = require('./helpers/run-browser');
 
+let readyCount = 0;
+// logging could happen before page.goto is finished, so
+// we need to wait for everything before closing browser
+const awaitManyBeforeClose = (expectedCount, browser, done) => {
+  readyCount += 1;
+  if (readyCount === expectedCount) {
+    readyCount = 0;
+    browser.close().then(done);
+  }
+};
+
 describe('Hot Module Replacement', () => {
   describe('simple hot config', () => {
     jest.setTimeout(30000);
@@ -20,14 +31,20 @@ describe('Hot Module Replacement', () => {
 
     afterAll(helper.close);
 
+    beforeEach(() => {
+      readyCount = 0;
+    });
+
     it('should log that it is using HMR to the console as first message', (done) => {
       runBrowser().then(({ page, browser }) => {
         page.once('console', (msg) => {
           expect(msg.args().length).toEqual(1);
           expect(msg.text()).toMatch(/\[HMR\]/);
-          browser.close().then(done);
+          awaitManyBeforeClose(2, browser, done);
         });
-        page.goto('http://localhost:9000/main');
+        page.goto('http://localhost:9000/main').then(() => {
+          awaitManyBeforeClose(2, browser, done);
+        });
       });
     });
   });
@@ -51,9 +68,11 @@ describe('Hot Module Replacement', () => {
         page.once('console', (msg) => {
           expect(msg.args().length).toEqual(1);
           expect(msg.text()).toMatch(/\[HMR\]/);
-          browser.close().then(done);
+          awaitManyBeforeClose(2, browser, done);
         });
-        page.goto('http://localhost:9000/main');
+        page.goto('http://localhost:9000/main').then(() => {
+          awaitManyBeforeClose(2, browser, done);
+        });
       });
     });
   });
@@ -77,9 +96,11 @@ describe('Hot Module Replacement', () => {
         page.once('console', (msg) => {
           expect(msg.args().length).toEqual(1);
           expect(msg.text()).toMatch(/Hey\./);
-          browser.close().then(done);
+          awaitManyBeforeClose(2, browser, done);
         });
-        page.goto('http://localhost:9000/main');
+        page.goto('http://localhost:9000/main').then(() => {
+          awaitManyBeforeClose(2, browser, done);
+        });
       });
     });
   });
