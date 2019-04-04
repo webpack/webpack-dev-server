dcd2434f3b9243395e91136cbded54203d202d87
Loonride <knagaitsev1@gmail.com>
2019-04-04 11:03:12 -0500

    feat: http2 option to enable/disable HTTP/2 with HTTPS (#1721)

--- test/CreateConfig.test.js
+++ test/CreateConfig.test.js
@@ -560,6 +560,28 @@ describe('createConfig', () => {
     expect(config).toMatchSnapshot();
   });
 
+  it('http2 option', () => {
+    const config = createConfig(
+      webpackConfig,
+      Object.assign({}, argv, { https: true, http2: true }),
+      { port: 8080 }
+    );
+
+    expect(config).toMatchSnapshot();
+  });
+
+  it('http2 option (in devServer config)', () => {
+    const config = createConfig(
+      Object.assign({}, webpackConfig, {
+        devServer: { https: true, http2: true },
+      }),
+      argv,
+      { port: 8080 }
+    );
+
+    expect(config).toMatchSnapshot();
+  });
+
   it('key option', () => {
     const config = createConfig(
       webpackConfig,
