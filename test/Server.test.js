919ff77fdf85c3c5835a4869cd976b52b648a36a
Yuta Hiroto <git@hiroppy.me>
2019-04-05 17:48:46 +0100

    fix(Server): validate express.static.mime.types (#1765)

--- test/Server.test.js
+++ test/Server.test.js
@@ -16,6 +16,55 @@ const allStats = [
 ];
 
 describe('Server', () => {
+  // issue: https://github.com/webpack/webpack-dev-server/issues/1724
+  describe('express.static.mine.types', () => {
+    beforeEach(() => {
+      jest.resetModules();
+    });
+
+    afterEach(() => {
+      jest.unmock('express');
+    });
+
+    it("should success even if mine.types doesn't exist", () => {
+      // eslint-disable-next-line
+      const Server = require('../lib/Server');
+
+      jest.mock('express', () => {
+        const data = jest.requireActual('express');
+        const { static: st } = data;
+        const { mime } = st;
+
+        delete mime.types;
+
+        expect(typeof mime.types).toEqual('undefined');
+
+        return Object.assign(data, {
+          static: Object.assign(st, {
+            mime,
+          }),
+        });
+      });
+
+      return new Promise((res) => {
+        const compiler = webpack(config);
+        const server = new Server(compiler);
+
+        compiler.hooks.done.tap('webpack-dev-server', (s) => {
+          const output = server.getStats(s);
+          expect(output.errors.length).toEqual(0);
+
+          server.close(() => {
+            res();
+          });
+        });
+
+        compiler.run(() => {});
+        server.listen(8080, 'localhost');
+      });
+    });
+  });
+
   it('should cascade warningsFilter', () => {
     const stats = { warningsFilter: 'test' };
     return new Promise((res) => {
