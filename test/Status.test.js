3e049d909c8efc5fdb259f67e9a5a1a80a813f87
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2019-04-08 14:19:58 -0400

    feat(open): modify `devServer.options` to take an object
    https://www.npmjs.com/package/open
    
    Allow for 3 options to pass as options.open:
    - Boolean: runs without any options and thus uses the default browser
    - String: opens with the browser of your choice
    - Object: takes a settings object as defined by https://github.com/sindresorhus/open
    
    As we are now doing now allowing for more complex (and possibly confusing) options to be passed in I have added logging aimed at making this more discoverable to a user.
    
    If they pass in an object with neither `app` nor `wait` OR something that is not ( boolean | string | object ) they will get a warning telling them that they passed in an invalid config and a link to the `open` docs.
    
    With that, I have also hard coded the open failure message, as config related info should now be caught earlier

---
+++ test/Status.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+jest.mock('opn');
+const open = require('opn');
+const status = require('../lib/utils/status');
+
+open.mockReturnValue('foo');
+const catchMock = jest.fn(() => 'foo');
+open.mockReturnValue({
+  catch: catchMock,
+});
+
+const uri = 'http://127.0.0.1';
+const log = {
+  info: jest.fn(),
+  warn: jest.fn(),
+};
+
+describe('status)', () => {
+  beforeEach(() => {
+    open.mockClear();
+    catchMock.mockClear();
+    log.info.mockClear();
+    log.warn.mockClear();
+  });
+  describe('open', () => {
+    it('should NOT call open if `open` is not passed in', () => {
+      const options = {};
+      status(uri, options, log, true);
+      expect(open).not.toBeCalled();
+    });
+    it('should call open with the URI passed in', () => {
+      const options = {
+        open: true,
+      };
+      status(uri, options, log, true);
+      expect(open).toBeCalledWith(uri, {});
+    });
+    it('should call open with no options if open:true', () => {
+      const options = {
+        open: true,
+      };
+      status(uri, options, log, true);
+      expect(open).toBeCalledWith(uri, {});
+    });
+    it('should call open with app:"app_name" is open is a string ', () => {
+      const options = {
+        open: 'Google Chrome',
+      };
+      status(uri, options, log, true);
+      expect(open).toBeCalledWith(uri, { app: 'Google Chrome' });
+    });
+    it('should call open with app:["app_name", "option"] is open is an array ', () => {
+      const options = {
+        open: ['Google Chrome', 'incognito'],
+      };
+      status(uri, options, log, true);
+      expect(open).toBeCalledWith(uri, { app: ['Google Chrome', 'incognito'] });
+    });
+  });
+});
