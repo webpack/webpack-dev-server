e395a9665bbb2b6e8b17abcaf31dc23cdfd9ed98
chaos-git <chaos-git@users.noreply.github.com>
2019-04-19 14:10:41 -0700

    Fixes #1805: Allow symlinks for devServer.https options (#1807)

--- test/Https.test.js
+++ test/Https.test.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const request = require('supertest');
 const helper = require('./helper');
 const config = require('./fixtures/contentbase-config/webpack.config');
+const skipTestOnWindows = require('./helpers/conditional-test')
+  .skipTestOnWindows;
 
 const httpsCertificateDirectory = path.join(
   __dirname,
@@ -93,6 +95,36 @@ describe('HTTPS', () => {
     it('Request to index', (done) => {
       req.get('/').expect(200, /Heyo/, done);
     });
+  });
+
+  describe('ca, pfx, key and cert are symlinks', () => {
+    if (skipTestOnWindows('Symlinks are not supported on Windows')) {
+      return;
+    }
+
+    beforeAll((done) => {
+      server = helper.start(
+        config,
+        {
+          contentBase: contentBasePublic,
+          https: {
+            ca: path.join(httpsCertificateDirectory, 'ca-symlink.pem'),
+            pfx: path.join(httpsCertificateDirectory, 'server-symlink.pfx'),
+            key: path.join(httpsCertificateDirectory, 'server-symlink.key'),
+            cert: path.join(httpsCertificateDirectory, 'server-symlink.crt'),
+            passphrase: 'webpack-dev-server',
+          },
+        },
+        done
+      );
+      req = request(server.app);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/').expect(200, /Heyo/, done);
+    });
+
+    afterAll(helper.close);
   });
 
   describe('ca, pfx, key and cert are raw strings', () => {
