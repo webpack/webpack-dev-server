04a08b918107dd19364fee8a2efd2d7772f46d79
Yuta Hiroto <git@about-hiroppy.com>
2019-04-28 01:05:44 +0100

    test(options): delete snapshot tests

--- test/options/options.test.js
+++ test/options/options.test.js
@@ -9,14 +9,6 @@ const config = require('../fixtures/simple-config/webpack.config');
 const options = require('../../lib/options.json');
 
 describe('options', () => {
-  it('should return properties', () => {
-    expect(options.properties).toMatchSnapshot();
-  });
-
-  it('should return errorMessage', () => {
-    expect(options.errorMessage.properties).toMatchSnapshot();
-  });
-
   it('should match properties and errorMessage', () => {
     const properties = Object.keys(options.properties);
     const messages = Object.keys(options.errorMessage.properties);
