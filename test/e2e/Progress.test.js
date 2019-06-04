4a51af23f5304b8632706c9b3c469e391c215ba2
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-06-03 22:43:03 -0500

    feat(server/client): made progress option available to API

---
+++ test/e2e/Progress.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/* eslint-disable
+  no-undef
+*/
+const fs = require('fs');
+const path = require('path');
+const testServer = require('../helpers/test-server');
+const reloadConfig = require('../fixtures/reload-config/webpack.config');
+const runBrowser = require('../helpers/run-browser');
+
+describe('client progress', () => {
+  describe('using hot', () => {
+    const cssFilePath = path.resolve(__dirname, '../temp/main.css');
+    beforeAll((done) => {
+      fs.writeFileSync(
+        cssFilePath,
+        'body { background-color: rgb(0, 0, 255); }'
+      );
+      const options = {
+        port: 9000,
+        host: '0.0.0.0',
+        inline: true,
+        hot: true,
+        progress: true,
+        watchOptions: {
+          poll: 500,
+        },
+      };
+      testServer.startAwaitingCompilation(reloadConfig, options, done);
+    });
+
+    afterAll((done) => {
+      testServer.close(done);
+    });
+
+    describe('on browser client', () => {
+      jest.setTimeout(30000);
+
+      it('should console.log progress', (done) => {
+        runBrowser().then(({ page, browser }) => {
+          const res = [];
+          page.waitForNavigation({ waitUntil: 'load' }).then(() => {
+            fs.writeFileSync(
+              cssFilePath,
+              'body { background-color: rgb(255, 0, 0); }'
+            );
+            page.waitFor(10000).then(() => {
+              expect(res).toMatchSnapshot();
+              browser.close().then(done);
+            });
+          });
+
+          page.goto('http://localhost:9000/main');
+          page.on('console', ({ _text }) => {
+            res.push(_text);
+          });
+        });
+      });
+    });
+  });
+});
