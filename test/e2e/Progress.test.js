357df68cfcf147a27573990f2f249d4b9d321ffb
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-06-15 12:37:45 -0500

    test(client): new progress plugin multi handler test

--- test/e2e/Progress.test.js
+++ test/e2e/Progress.test.js
@@ -48,15 +48,14 @@ describe('client progress', () => {
             );
             page.waitFor(10000).then(() => {
               browser.close().then(() => {
-                // console.log(res);
-                // // check that there is some percentage progress output
-                // const regExp = /^\[WDS\] [0-9]{1,3}% - /;
-                // const match = res.find((line) => {
-                //   return regExp.test(line);
-                // });
-                // // eslint-disable-next-line no-undefined
-                // expect(match).not.toEqual(undefined);
-                expect(res).toMatchSnapshot();
+                console.log(res);
+                // check that there is some percentage progress output
+                const regExp = /^\[WDS\] [0-9]{1,3}% - /;
+                const match = res.find((line) => {
+                  return regExp.test(line);
+                });
+                // eslint-disable-next-line no-undefined
+                expect(match).not.toEqual(undefined);
                 done();
               });
             });
