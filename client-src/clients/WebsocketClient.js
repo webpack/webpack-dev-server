c6ee4d5f232271c5bc8585fbb2ec8daa75ff52e3
Loonride <knagaitsev1@gmail.com>
2019-06-25 04:12:19 -0500

    feat(experimentally): websocket client implementation (#2058)

--- client-src/clients/WebsocketClient.js
+++ client-src/clients/WebsocketClient.js
@@ -1,5 +1,34 @@
 'use strict';
 
+/* global WebSocket */
+
+/* eslint-disable
+  no-unused-vars
+*/
 const BaseClient = require('./BaseClient');
 
-module.exports = class WebsocketClient extends BaseClient {};
+module.exports = class WebsocketClient extends BaseClient {
+  constructor(url) {
+    super();
+    this.client = new WebSocket(url.replace(/^http/, 'ws'));
+  }
+
+  static getClientPath(options) {
+    return require.resolve('./WebsocketClient');
+  }
+
+  onOpen(f) {
+    this.client.onopen = f;
+  }
+
+  onClose(f) {
+    this.client.onclose = f;
+  }
+
+  // call f with the message string as the first argument
+  onMessage(f) {
+    this.client.onmessage = (e) => {
+      f(e.data);
+    };
+  }
+};
