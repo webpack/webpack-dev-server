c95ba88bc78d88812ef54f9e0a185fc7e1b7dfca
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-07-12 15:05:48 -0500

    test(cli): switch to spawn for stdin test

--- test/cli/cli.test.js
+++ test/cli/cli.test.js
@@ -122,6 +122,51 @@ describe('CLI', () => {
       .catch(done);
   });
 
+  it('without --stdin, with stdin "end" event should time out', (done) => {
+    const configPath = resolve(
+      __dirname,
+      '../fixtures/simple-config/webpack.config.js'
+    );
+    const childProcess = testBin(false, configPath, true);
+
+    setTimeout(() => {
+      // this is meant to confirm that it does not have any effect on the running process
+      // since options.stdin is not enabled
+      childProcess.stdin.emit('end');
+    }, 500);
+
+    setTimeout(() => {
+      childProcess.kill();
+    }, 1000);
+
+    childProcess.once('close', (code, signal) => {
+      expect(childProcess.killed).toBeTruthy();
+      expect(signal).toEqual('SIGTERM');
+      done();
+    });
+  });
+
+  it('--stdin, with "end" event should exit without time out', (done) => {
+    const configPath = resolve(
+      __dirname,
+      '../fixtures/simple-config/webpack.config.js'
+    );
+    const childProcess = testBin('--stdin', configPath);
+
+    setTimeout(() => {
+      childProcess.stdin.emit('end');
+    }, 500);
+
+    childProcess
+      .then((output) => {
+        expect(output.code).toEqual(0);
+        expect(output.timedOut).toBeFalsy();
+        expect(output.killed).toBeFalsy();
+        done();
+      })
+      .catch(done);
+  });
+
   it('should accept the promise function of webpack.config.js', (done) => {
     testBin(
       false,
