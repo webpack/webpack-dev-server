6ef06cdd863f502fdec0330d73a744e1e734ddd5
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-07-18 19:30:53 -0500

    test(e2e): space out when e2e tests are run due to concurrency

--- testSequencer.js
+++ testSequencer.js
@@ -5,17 +5,38 @@ const Sequencer = require('@jest/test-sequencer').default;
 class CustomSequencer extends Sequencer {
   sort(tests) {
     const copyTests = Array.from(tests);
+    const len = copyTests.length;
+    const endToEndTests = copyTests.filter(test => test.path.includes('/e2e/'));
+    const nonEndToEndTests = copyTests.filter(test => !test.path.includes('/e2e/'));
+    
+    // bring the most unstable test to the front
     const str = 'e2e/Client.test.js';
-    return copyTests.sort((testA, testB) => {
-        if (testA.path.includes(str)) {
-            return -1;
-        }
-        else if (testB.path.includes(str)) {
-            return 1;
-        }
+    endToEndTests.sort((testA, testB) => {
+      if (testB.path.includes(str)) {
+        return 1;
+      }
 
-        return 0;
+      return -1;
     });
+
+    const res = [];
+
+    const spacing = Math.ceil(len / endToEndTests.length);
+
+    let endToEndIndex = 0;
+    let nonEndToEndIndex = 0;
+    for (let i = 0; i < len; i++) {
+      if (i % spacing === 0 && endToEndIndex < endToEndTests.length) {
+        res.push(endToEndTests[endToEndIndex]);
+        endToEndIndex++;
+      }
+      else if (nonEndToEndIndex < nonEndToEndTests.length) {
+        res.push(nonEndToEndTests[nonEndToEndIndex]);
+        nonEndToEndIndex++;
+      }
+    }
+
+    return res;
   }
 }
 
