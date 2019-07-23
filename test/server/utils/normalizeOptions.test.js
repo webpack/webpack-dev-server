7e37db5b5384ddcacc3168ec952a725a3b14ed21
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-07-23 16:29:43 -0500

    refactor(server): switched to normalize options

--- test/server/utils/normalizeOptions.test.js
+++ test/server/utils/normalizeOptions.test.js
@@ -37,17 +37,124 @@ describe('normalizeOptions', () => {
       },
       optionsResults: null,
     },
+    {
+      title: 'no devServer.filename, no compiler filename',
+      webpackConfig: null,
+      multiCompiler: false,
+      options: {},
+      optionsResults: null,
+    },
+    {
+      title: 'no devServer.filename, existing compiler filename',
+      webpackConfig: {
+        output: {
+          filename: 'mybundle.js',
+        },
+      },
+      multiCompiler: false,
+      options: {},
+      optionsResults: null,
+    },
+    {
+      title: 'existing devServer.filename, no compiler filename',
+      webpackConfig: null,
+      multiCompiler: false,
+      options: {
+        filename: 'devserver-bundle.js',
+      },
+      optionsResults: null,
+    },
+    {
+      title: 'existing devServer.filename, existing compiler filename',
+      webpackConfig: {
+        output: {
+          filename: 'mybundle.js',
+        },
+      },
+      multiCompiler: false,
+      options: {
+        filename: 'devserver-bundle.js',
+      },
+      optionsResults: null,
+    },
+    {
+      title: 'multi compiler, no devServer.filename, no compiler filename',
+      webpackConfig: null,
+      multiCompiler: true,
+      options: {},
+      optionsResults: null,
+    },
+    {
+      title:
+        'multi compiler, no devServer.filename, existing compiler filename',
+      webpackConfig: {
+        output: {
+          filename: 'mybundle.js',
+        },
+      },
+      multiCompiler: true,
+      options: {},
+      optionsResults: null,
+    },
+    {
+      title:
+        'multi compiler, existing devServer.filename, no compiler filename',
+      webpackConfig: null,
+      multiCompiler: true,
+      options: {
+        filename: 'devserver-bundle.js',
+      },
+      optionsResults: null,
+    },
+    {
+      title:
+        'multi compiler, existing devServer.filename, existing compiler filename',
+      webpackConfig: {
+        output: {
+          filename: 'mybundle.js',
+        },
+      },
+      multiCompiler: true,
+      options: {
+        filename: 'devserver-bundle.js',
+      },
+      optionsResults: null,
+    },
   ];
 
   cases.forEach((data) => {
     describe(data.title, () => {
       let compiler;
       beforeAll(() => {
+        // this will merge webpack configs through a depth of one layer of objects,
+        // specifically so that the webpack config output object can be merged
+        const mergeConfigs = (baseConfig, config) => {
+          Object.keys(config).forEach((key1) => {
+            if (typeof config[key1] === 'object') {
+              Object.keys(config[key1]).forEach((key2) => {
+                if (!baseConfig[key1]) {
+                  baseConfig[key1] = {};
+                }
+                baseConfig[key1][key2] = config[key1][key2];
+              });
+            } else {
+              baseConfig[key1] = config[key1];
+            }
+          });
+        };
+
         let webpackConfig;
         if (data.multiCompiler) {
           webpackConfig = require('../../fixtures/multi-compiler-config/webpack.config');
         } else {
           webpackConfig = require('../../fixtures/simple-config/webpack.config');
+        }
+
+        if (data.webpackConfig) {
+          mergeConfigs(
+            data.multiCompiler ? webpackConfig[0] : webpackConfig,
+            data.webpackConfig
+          );
         }
 
         compiler = webpack(webpackConfig);
