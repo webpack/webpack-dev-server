146b0a85098ba5bef53881b49c7177a2962f163c
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-08-09 22:38:53 -0500

    feat(server): add stdin for api

---
+++ test/server/stdin-option.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const config = require('../fixtures/simple-config/webpack.config');
+const testServer = require('../helpers/test-server');
+const port = require('../ports-map')['stdin-option'];
+
+describe('stdin', () => {
+  // eslint-disable-next-line no-unused-vars
+  let server;
+  let exitSpy;
+
+  beforeAll(() => {
+    exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+  });
+
+  afterEach((done) => {
+    server = null;
+    exitSpy.mockReset();
+    process.stdin.removeAllListeners('end');
+    testServer.close(done);
+  });
+
+  describe('enabled', () => {
+    beforeAll((done) => {
+      server = testServer.start(
+        config,
+        {
+          port,
+          stdin: true,
+        },
+        done
+      );
+    });
+
+    it('should exit process', (done) => {
+      process.stdin.emit('end');
+      setTimeout(() => {
+        process.stdin.pause();
+        expect(exitSpy.mock.calls[0]).toEqual([0]);
+        done();
+      }, 1000);
+    });
+  });
+
+  describe('disabled (default)', () => {
+    beforeAll((done) => {
+      server = testServer.start(
+        config,
+        {
+          port,
+        },
+        done
+      );
+    });
+
+    it('should not exit process', (done) => {
+      process.stdin.emit('end');
+      setTimeout(() => {
+        process.stdin.pause();
+        expect(exitSpy.mock.calls.length).toEqual(0);
+        done();
+      }, 1000);
+    });
+  });
+});
---
+++ test/server/utils/handleStdin.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const handleStdin = require('../../../lib/utils/handleStdin');
+
+describe('handleStdin', () => {
+  let exitSpy;
+
+  beforeAll(() => {
+    exitSpy = jest.spyOn(process, 'exit').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    process.stdin.removeAllListeners('end');
+    exitSpy.mockReset();
+  });
+
+  describe('enabled', () => {
+    it('should exit process', (done) => {
+      handleStdin({
+        stdin: true,
+      });
+      process.stdin.emit('end');
+      setTimeout(() => {
+        process.stdin.pause();
+        expect(exitSpy.mock.calls[0]).toEqual([0]);
+        done();
+      }, 1000);
+    });
+  });
+
+  describe('disabled (default)', () => {
+    it('should not exit process', (done) => {
+      handleStdin({});
+      process.stdin.emit('end');
+      setTimeout(() => {
+        process.stdin.pause();
+        expect(exitSpy.mock.calls.length).toEqual(0);
+        done();
+      }, 1000);
+    });
+  });
+});
