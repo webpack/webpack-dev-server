6159956caf68fb0ac30a7b53a5dab9aeb897aff6
Kirill Nagaitsev <knagaitsev1@gmail.com>
2019-08-22 18:26:32 -0500

    feat(server): pass errors to onListening in error first callback style

--- test/server/onListening-option.test.js
+++ test/server/onListening-option.test.js
@@ -1,21 +1,27 @@
 'use strict';
 
-const { unlink } = require('fs');
 const { join } = require('path');
+const { unlinkAsync } = require('../helpers/fs');
 const testServer = require('../helpers/test-server');
 const config = require('../fixtures/simple-config/webpack.config');
 const port = require('../ports-map')['onListening-option'];
 const { skipTestOnWindows } = require('../helpers/conditional-test');
+const TestUnixSocket = require('../helpers/test-unix-socket');
 
 describe('onListening option', () => {
   describe('with host and port', () => {
     let onListeningIsRunning = false;
+    let server;
+    let resultingDevServer;
+    let resultingErr;
 
     beforeAll((done) => {
-      testServer.start(
+      server = testServer.start(
         config,
         {
-          onListening: (devServer) => {
+          onListening: (err, devServer) => {
+            resultingErr = err;
+            resultingDevServer = devServer;
             if (!devServer) {
               throw new Error('webpack-dev-server is not defined');
             }
@@ -30,7 +36,9 @@ describe('onListening option', () => {
 
     afterAll(testServer.close);
 
-    it('should run onListening callback', () => {
+    it('should run onListening callback, providing server without error', () => {
+      expect(resultingErr).toBeUndefined();
+      expect(resultingDevServer).toEqual(server);
       expect(onListeningIsRunning).toBe(true);
     });
   });
@@ -42,12 +50,17 @@ describe('onListening option', () => {
 
     const socketPath = join('.', 'onListening.webpack.sock');
     let onListeningIsRunning = false;
+    let server;
+    let resultingDevServer;
+    let resultingErr;
 
     beforeAll((done) => {
-      testServer.start(
+      server = testServer.start(
         config,
         {
-          onListening: (devServer) => {
+          onListening: (err, devServer) => {
+            resultingErr = err;
+            resultingDevServer = devServer;
             if (!devServer) {
               throw new Error('webpack-dev-server is not defined');
             }
@@ -62,10 +75,64 @@ describe('onListening option', () => {
 
     afterAll(testServer.close);
 
-    it('should run onListening callback', (done) => {
+    it('should run onListening callback, providing server without error', async () => {
+      expect(resultingErr).toBeUndefined();
+      expect(resultingDevServer).toEqual(server);
       expect(onListeningIsRunning).toBe(true);
 
-      unlink(socketPath, done);
+      await unlinkAsync(socketPath);
+    });
+  });
+
+  describe('with Unix socket that is already in use', () => {
+    if (skipTestOnWindows('Unix sockets are not supported on Windows')) {
+      return;
+    }
+
+    const socketPath = join('.', 'onListening.webpack.sock');
+    let onListeningIsRunning = false;
+    let server;
+    let resultingDevServer;
+    let resultingErr;
+    let testUnixSocket;
+
+    beforeAll((done) => {
+      testUnixSocket = new TestUnixSocket();
+      // this unix socket is meant to use the file that the dev server
+      // will try to listen on, causing an error to be passed to onListening
+      testUnixSocket.server.listen(socketPath, 511, () => {
+        server = testServer.start(
+          config,
+          {
+            onListening: (err, devServer) => {
+              resultingErr = err;
+              resultingDevServer = devServer;
+              if (!devServer) {
+                throw new Error('webpack-dev-server is not defined');
+              }
+
+              onListeningIsRunning = true;
+            },
+            socket: socketPath,
+          },
+          done
+        );
+      });
+    });
+
+    afterAll((done) => {
+      testUnixSocket.close(() => {
+        testServer.close(done);
+      });
+    });
+
+    it('should run onListening callback, providing server without error', async () => {
+      expect(resultingErr).not.toBeUndefined();
+      expect(resultingErr.message).toEqual('This socket is already used');
+      expect(resultingDevServer).toEqual(server);
+      expect(onListeningIsRunning).toBe(true);
+
+      await unlinkAsync(socketPath);
     });
   });
 });
