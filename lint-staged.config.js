984536c12b25fc182362bbc9ba26c474e2fb5b10
James George <jamesgeorge998001@gmail.com>
2020-04-20 05:35:26 +0530

    chore: update lint-staged config (#2524)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  '*.js': ['prettier --write', 'eslint --fix', 'git add'],
-  '*.{json,md,yml,css}': ['prettier --write', 'git add'],
+  '*.js': ['prettier --write', 'eslint --fix'],
+  '*.{json,md,yml,css}': ['prettier --write'],
 };
