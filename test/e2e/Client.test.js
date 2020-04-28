0c744470a4d3364af1cf0af8dfe2ddf2d293e476
Yuta Hiroto <git@hiroppy.me>
2020-04-28 23:31:23 +0900

    test: fix e2e/client

--- test/e2e/Client.test.js
+++ test/e2e/Client.test.js
@@ -106,15 +106,18 @@ describe('reload', () => {
                             const bgColor = getComputedStyle(body)[
                               'background-color'
                             ];
+
                             return bgColor;
                           })
                           .then((color2) => {
-                            browser.close().then(() => {
-                              expect(color).toEqual('rgb(0, 0, 255)');
-                              expect(color2).toEqual('rgb(255, 0, 0)');
-                              expect(refreshed).toEqual(mode.shouldRefresh);
-                              done();
-                            });
+                            expect(color).toEqual('rgb(0, 0, 255)');
+                            expect(color2).toEqual('rgb(255, 0, 0)');
+                            expect(refreshed).toEqual(mode.shouldRefresh);
+
+                            return browser.close();
+                          })
+                          .then(() => {
+                            done();
                           });
                       });
                     });
