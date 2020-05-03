3133bb55c9fa2c40e7387e00eb2fd2adc9868c63
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-05-03 12:37:20 -0500

    chore(scripts): move webpack build input and output to config files

--- client-src/default/webpack.config.js
+++ client-src/default/webpack.config.js
@@ -1,7 +1,14 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
   mode: 'production',
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, '../../client/default'),
+    filename: 'index.bundle.js',
+  },
   module: {
     rules: [
       {
--- client-src/sockjs/webpack.config.js
+++ client-src/sockjs/webpack.config.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
   mode: 'production',
+  entry: path.join(__dirname, 'index.js'),
   output: {
+    path: path.resolve(__dirname, '../../client/sockjs'),
+    filename: 'sockjs.bundle.js',
     library: 'SockJS',
     libraryTarget: 'umd',
   },
