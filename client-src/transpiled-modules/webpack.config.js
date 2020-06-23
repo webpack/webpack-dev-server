19ebe175ba42dadea0ab183a48c3c87422044e9d
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-06-23 18:42:32 -0500

    refactor(client): make log and strip ansi transpiled bundle modules

---
+++ client-src/transpiled-modules/webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const merge = require('webpack-merge');
+
+const base = {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../../client/transpiled-modules'),
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = [
+  merge(base, {
+    entry: path.join(__dirname, 'log.js'),
+    output: {
+      filename: 'log.js',
+    },
+  }),
+  merge(base, {
+    entry: path.join(__dirname, 'strip-ansi.js'),
+    output: {
+      filename: 'strip-ansi.js',
+    },
+  }),
+];
