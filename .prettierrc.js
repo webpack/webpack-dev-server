6e18c8f8ee8a6dff0d6f3b3781d46e4a66163bec
Yuta Hiroto <git@hiroppy.me>
2020-09-12 11:16:59 +0900

    chore(prettier): delete default options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,7 @@
+'use strict';
+
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'es5',
-  arrowParens: 'always',
   overrides: [
     {
       files: '*.json',
