f037beb09507b5882a531958dc08ac0c028e8414
Reinis Adovičs / kroko <code@kroko.me>
2020-12-01 15:15:11 +0200

    feat(client): file URLs in WebSocket URL creation
    Co-authored-by: ylemkimon <y@ylem.kim>

--- client-src/clients/WebsocketClient.js
+++ client-src/clients/WebsocketClient.js
@@ -8,14 +8,7 @@ const BaseClient = require('./BaseClient');
 module.exports = class WebsocketClient extends BaseClient {
   constructor(url) {
     super();
-    this.client = new WebSocket(
-      ((urlToChange) => {
-        let wsUrl = urlToChange;
-        wsUrl = wsUrl.replace(/^http/, 'ws');
-        wsUrl = wsUrl.replace(/^file/, 'ws');
-        return wsUrl;
-      })(url)
-    );
+    this.client = new WebSocket(url.replace(/^(?:http|file)/, 'ws'));
 
     this.client.onerror = (err) => {
       log.error(err);
