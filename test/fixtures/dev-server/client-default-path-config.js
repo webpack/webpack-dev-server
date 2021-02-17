4f646db38b97857dc07f51cfa123fd3290e8789e
Nitin Kumar <snitin315@gmail.com>
2021-02-17 19:43:08 +0530

    test: refactor

---
+++ test/fixtures/dev-server/client-default-path-config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+module.exports = {
+    mode: 'development',
+    stats: 'detailed',
+    entry: resolve(__dirname, './foo.js'),
+    devServer: {
+        client: {
+            path: '/ws',
+        },
+        transportMode: {
+          server: 'sockjs',
+          client: 'sockjs',
+        },
+    }
+}
---
+++ test/fixtures/dev-server/default-config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+module.exports = {
+    mode: 'development',
+    stats: 'detailed',
+    entry: resolve(__dirname, './foo.js'),
+    devServer: {
+        client: {
+            path: '/custom/path',
+        },
+        transportMode: {
+          server: 'sockjs',
+          client: 'sockjs',
+        },
+    }
+}
