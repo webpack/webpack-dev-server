2d923e37158a9761286f7a8c72df50dc888ed100
Anshuman Verma <anshu.av97@gmail.com>
2021-03-16 01:57:48 +0530

    fix: migrate flags to new configs format

--- bin/cli-flags.js
+++ bin/cli-flags.js
@@ -4,50 +4,85 @@ module.exports = {
   devServer: [
     {
       name: 'host',
-      type: String,
+      configs: [
+        {
+          type: 'string',
+        },
+      ],
       description: 'The hostname/ip address the server will bind to.',
     },
     {
       name: 'port',
-      type: Number,
+      configs: [
+        {
+          type: 'number',
+        },
+      ],
       description: 'The port server will listen to.',
     },
     {
       name: 'static',
-      type: [String, Boolean],
+      configs: [
+        {
+          type: 'string',
+        },
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'A directory to serve static content from.',
       multiple: true,
       negative: true,
     },
     {
       name: 'live-reload',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Enables live reloading on changing files.',
       negatedDescription: 'Disables live reloading on changing files.',
       negative: true,
     },
     {
       name: 'https',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Use HTTPS protocol.',
       negatedDescription: 'Do not use HTTPS protocol.',
       negative: true,
     },
     {
       name: 'http2',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Use HTTP/2, must be used with HTTPS.',
       negatedDescription: 'Do not use HTTP/2.',
       negative: true,
     },
     {
       name: 'bonjour',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Broadcasts the server via ZeroConf networking on start.',
     },
     {
       name: 'client-progress',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Print compilation progress in percentage in the browser.',
       processor(opts) {
         opts.client = opts.client || {};
@@ -57,7 +92,11 @@ module.exports = {
     },
     {
       name: 'setup-exit-signals',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Close and exit the process on SIGINT and SIGTERM.',
       negatedDescription:
         'Do not close and exit the process on SIGNIT and SIGTERM.',
@@ -65,29 +104,52 @@ module.exports = {
     },
     {
       name: 'stdin',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Close when stdin ends.',
     },
     {
       name: 'open',
-      type: [String, Boolean],
+      configs: [
+        {
+          type: 'boolean',
+        },
+        {
+          type: 'string',
+        },
+      ],
       description:
         'Open the default browser, or optionally specify a browser name.',
     },
     {
       name: 'use-local-ip',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Open default browser with local IP.',
     },
     {
       name: 'open-page',
-      type: String,
+      configs: [
+        {
+          type: 'string',
+        },
+      ],
       description: 'Open default browser with the specified page.',
       multiple: true,
     },
     {
       name: 'client-logging',
-      type: String,
+      configs: [
+        {
+          type: 'string',
+        },
+      ],
       description:
         'Log level in the browser (none, error, warn, info, log, verbose).',
       processor(opts) {
@@ -98,7 +160,11 @@ module.exports = {
     },
     {
       name: 'history-api-fallback',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Fallback to /index.html for Single Page Applications.',
       negatedDescription:
         'Do not fallback to /index.html for Single Page Applications.',
@@ -106,19 +172,34 @@ module.exports = {
     },
     {
       name: 'compress',
-      type: Boolean,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'Enable gzip compression.',
       negatedDescription: 'Disable gzip compression.',
       negative: true,
     },
     {
       name: 'public',
-      type: String,
+      configs: [
+        {
+          type: 'boolean',
+        },
+      ],
       description: 'The public hostname/ip address of the server.',
     },
     {
       name: 'firewall',
-      type: [String, Boolean],
+      configs: [
+        {
+          type: 'boolean',
+        },
+        {
+          type: 'string',
+        },
+      ],
       description:
         'Enable firewall or set hosts that are allowed to access the dev server.',
       negatedDescription: 'Disable firewall.',
