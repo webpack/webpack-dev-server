7195b359f9828ab04ca1abbfff514b2e00e67c15
Anshuman Verma <anshu.av97@gmail.com>
2021-04-30 02:09:10 +0530

    refactor: utils (#3207)

--- test/server/utils/defaultPort.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const defaultPort = require('../../../lib/utils/defaultPort');
-
-describe('defaultPort util', () => {
-  it('should return value', () => {
-    expect(defaultPort).toBe(8080);
-  });
-});
