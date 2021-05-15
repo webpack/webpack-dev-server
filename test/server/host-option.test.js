3ca0ad21c3b87a6eb2052532d7c95ad8a44c7c29
Nitin Kumar <snitin315@gmail.com>
2021-05-15 17:44:22 +0530

    test: add more cases for `host` option

--- test/server/host-option.test.js
+++ test/server/host-option.test.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const request = require('supertest');
+const internalIp = require('internal-ip');
 const config = require('../fixtures/simple-config/webpack.config');
 const testServer = require('../helpers/test-server');
 const port = require('../ports-map')['host-option'];
@@ -191,6 +192,70 @@ describe('host option', () => {
       const address = server.server.address();
 
       expect(address.address).toBe('0.0.0.0');
+      expect(address.port).toBe(port);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/').expect(200, done);
+    });
+
+    afterAll(testServer.close);
+  });
+
+  describe('is local-ip', () => {
+    beforeAll((done) => {
+      server = testServer.start(
+        config,
+        {
+          static: {
+            directory: staticDirectory,
+            watch: false,
+          },
+          host: 'local-ip',
+          port,
+        },
+        done
+      );
+      req = request(server.app);
+    });
+
+    it('server address', () => {
+      const address = server.server.address();
+      const networkIP = internalIp.v4.sync();
+
+      expect(address.address).toBe(networkIP);
+      expect(address.port).toBe(port);
+    });
+
+    it('Request to index', (done) => {
+      req.get('/').expect(200, done);
+    });
+
+    afterAll(testServer.close);
+  });
+
+  describe('is local-ipv4', () => {
+    beforeAll((done) => {
+      server = testServer.start(
+        config,
+        {
+          static: {
+            directory: staticDirectory,
+            watch: false,
+          },
+          host: 'local-ipv4',
+          port,
+        },
+        done
+      );
+      req = request(server.app);
+    });
+
+    it('server address', () => {
+      const address = server.server.address();
+      const networkIP = internalIp.v4.sync();
+
+      expect(address.address).toBe(networkIP);
       expect(address.port).toBe(port);
     });
 
