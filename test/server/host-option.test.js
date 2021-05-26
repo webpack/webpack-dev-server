638b3bcef4d6e71f1905efa44a251e3f0a3d2817
evilebottnawi <sheo13666q@gmail.com>
2021-05-26 18:38:13 +0300

    test: fix

--- test/server/host-option.test.js
+++ test/server/host-option.test.js
@@ -78,37 +78,6 @@ describe('host option', () => {
     afterAll(testServer.close);
   });
 
-  describe('is null', () => {
-    beforeAll((done) => {
-      server = testServer.start(
-        config,
-        {
-          static: {
-            directory: staticDirectory,
-            watch: false,
-          },
-          host: null,
-          port,
-        },
-        done
-      );
-      req = request(server.app);
-    });
-
-    it('server address', () => {
-      const address = server.server.address();
-
-      expect(address.address).toBe('::');
-      expect(address.port).toBe(port);
-    });
-
-    it('Request to index', (done) => {
-      req.get('/').expect(200, done);
-    });
-
-    afterAll(testServer.close);
-  });
-
   describe('is 127.0.0.1 (IPv4)', () => {
     beforeAll((done) => {
       server = testServer.start(
