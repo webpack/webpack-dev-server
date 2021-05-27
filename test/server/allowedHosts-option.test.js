033de4cff92d34ec605c181c47bc912ba2788256
evilebottnawi <sheo13666q@gmail.com>
2021-05-28 00:28:54 +0300

    fix: logic

--- test/server/allowedHosts-option.test.js
+++ test/server/allowedHosts-option.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const internalIp = require('internal-ip');
 const webpack = require('webpack');
 const Server = require('../../lib/Server');
 const config = require('../fixtures/simple-config/webpack.config');
@@ -20,14 +21,59 @@ describe('allowedHosts', () => {
     });
   });
 
-  it('should always allow any host if options.allowedHosts is enabled', () => {
+  it('should always allow `localhost` if options.allowedHosts is auto', () => {
     const options = {
+      allowedHosts: 'auto',
+    };
+    const headers = {
+      host: 'localhost',
+    };
+
+    server = createServer(compiler, options);
+
+    if (!server.checkHost(headers)) {
+      throw new Error("Validation didn't fail");
+    }
+  });
+
+  it('should always allow value from the `host` options if options.allowedHosts is auto', () => {
+    const networkIP = internalIp.v4.sync();
+    const options = {
+      host: networkIP,
+    };
+    const headers = {
+      host: networkIP,
+    };
+
+    server = createServer(compiler, options);
+
+    if (!server.checkHost(headers)) {
+      throw new Error("Validation didn't fail");
+    }
+  });
+
+  it('should always allow value of the `host` option from the `client.webSocketURL` option if options.allowedHosts is auto', () => {
+    const options = {
+      allowedHosts: 'auto',
       client: {
         webSocketURL: 'ws://test.host:80',
       },
-      allowedHosts: 'all',
+    };
+    const headers = {
+      host: 'test.host',
     };
 
+    server = createServer(compiler, options);
+
+    if (!server.checkHost(headers)) {
+      throw new Error("Validation didn't fail");
+    }
+  });
+
+  it('should always allow any host if options.allowedHosts is all', () => {
+    const options = {
+      allowedHosts: 'all',
+    };
     const headers = {
       host: 'bad.host',
     };
@@ -42,9 +88,11 @@ describe('allowedHosts', () => {
   it('should allow hosts in allowedHosts', () => {
     const tests = ['test.host', 'test2.host', 'test3.host'];
     const options = { allowedHosts: tests };
+
     server = createServer(compiler, options);
     tests.forEach((test) => {
       const headers = { host: test };
+
       if (!server.checkHost(headers)) {
         throw new Error("Validation didn't fail");
       }
@@ -53,7 +101,9 @@ describe('allowedHosts', () => {
 
   it('should allow hosts that pass a wildcard in allowedHosts', () => {
     const options = { allowedHosts: ['.example.com'] };
+
     server = createServer(compiler, options);
+
     const tests = [
       'www.example.com',
       'subdomain.example.com',
@@ -62,8 +112,10 @@ describe('allowedHosts', () => {
       'example.com:80',
       'subdomain.example.com:80',
     ];
+
     tests.forEach((test) => {
       const headers = { host: test };
+
       if (!server.checkHost(headers)) {
         throw new Error("Validation didn't fail");
       }
