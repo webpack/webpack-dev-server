33e7b6638ff4f9392ea394aeb36df78c3765addb
Anshuman Verma <anshu.av97@gmail.com>
2021-06-08 01:17:05 +0530

    chore: try jsdom

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/bin/this/process-arguments.js'],
   moduleFileExtensions: ['js', 'json'],
   testMatch: ['**/test/**/*.test.js'],
+  testEnvironment: 'jsdom',
   snapshotResolver: '<rootDir>/test/helpers/snapshotResolver.js',
   setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
   globalSetup: '<rootDir>/globalSetupTest.js',
