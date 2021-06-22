c2659191485b735526e7ad26a38930589ffa32fc
evilebottnawi <sheo13666q@gmail.com>
2021-06-22 15:40:47 +0300

    refactor: tests

--- test/server/onBeforeSetupMiddleware-option.test.js
+++ test/server/onBeforeSetupMiddleware-option.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
+const webpack = require('webpack');
 const request = require('supertest');
-const testServer = require('../helpers/test-server');
+const Server = require('../../lib/Server');
 const config = require('../fixtures/simple-config/webpack.config');
 const port = require('../ports-map')['on-before-setup-middleware-option'];
 
@@ -9,36 +10,58 @@ describe('onBeforeSetupMiddleware option', () => {
   let server;
   let req;
 
-  beforeAll((done) => {
-    server = testServer.start(
-      config,
+  beforeAll(async () => {
+    const compiler = webpack(config);
+
+    server = new Server(
       {
-        onBeforeSetupMiddleware: ({ app, compiler }) => {
-          if (!app) {
+        onBeforeSetupMiddleware: (self) => {
+          if (!self.app) {
             throw new Error('app is not defined');
           }
 
-          if (!compiler) {
+          if (!self.compiler) {
             throw new Error('compiler is not defined');
           }
 
-          app.get('/before/some/path', (_, response) => {
+          self.app.get('/before/some/path', (_, response) => {
             response.send('before');
           });
         },
         port,
       },
-      done
+      compiler
     );
+
+    await new Promise((resolve, reject) => {
+      server.listen(port, '127.0.0.1', (error) => {
+        if (error) {
+          reject(error);
+
+          return;
+        }
+
+        resolve();
+      });
+    });
     req = request(server.app);
   });
 
-  afterAll(testServer.close);
+  afterAll(async () => {
+    await new Promise((resolve) => {
+      server.close(() => {
+        resolve();
+      });
+    });
+  });
 
   it('should handle before route', async () => {
-    const res = await req.get('/before/some/path');
-    expect(res.headers['content-type']).toEqual('text/html; charset=utf-8');
-    expect(res.status).toEqual(200);
-    expect(res.text).toBe('before');
+    const response = await req.get('/before/some/path');
+
+    expect(response.headers['content-type']).toEqual(
+      'text/html; charset=utf-8'
+    );
+    expect(response.status).toEqual(200);
+    expect(response.text).toBe('before');
   });
 });
