fbd9fd38edac19f6480ba5dfbe75eb17f6b96858
evilebottnawi <sheo13666q@gmail.com>
2021-06-24 02:53:45 +0300

    fix: windows supports

--- test/e2e/ipc.test.js
+++ test/e2e/ipc.test.js
@@ -17,15 +17,7 @@ describe('web socket server URL', () => {
   for (const webSocketServer of webSocketServers) {
     const websocketURLProtocol = webSocketServer === 'ws' ? 'ws' : 'http';
 
-    const ipc = path.resolve(os.tmpdir(), 'webpack-dev-server.socket');
-
     it(`should work with the "ipc" option using "true" value ("${webSocketServer}")`, async () => {
-      try {
-        fs.unlinkSync(ipc);
-      } catch (_error) {
-        // Ignore
-      }
-
       const devServerHost = '127.0.0.1';
       const proxyHost = devServerHost;
       const proxyPort = port1;
@@ -52,7 +44,7 @@ describe('web socket server URL', () => {
 
       function startProxy(callback) {
         const proxy = httpProxy.createProxyServer({
-          target: { socketPath: '/tmp/webpack-dev-server.socket' },
+          target: { socketPath: server.options.ipc },
         });
 
         const proxyServer = http.createServer((request, response) => {
@@ -134,11 +126,11 @@ describe('web socket server URL', () => {
     });
 
     it(`should work with the "ipc" option using "string" value ("${webSocketServer}")`, async () => {
-      try {
-        fs.unlinkSync(ipc);
-      } catch (_error) {
-        // Ignore
-      }
+      const isWindows = process.platform === 'win32';
+      const localRelative = path.relative(process.cwd(), `${os.tmpdir()}/`);
+      const pipePrefix = isWindows ? '\\\\.\\pipe\\' : localRelative;
+      const pipeName = `webpack-dev-server.custom.sock`;
+      const ipc = path.join(pipePrefix, pipeName);
 
       const devServerHost = '127.0.0.1';
       const proxyHost = devServerHost;
@@ -166,7 +158,123 @@ describe('web socket server URL', () => {
 
       function startProxy(callback) {
         const proxy = httpProxy.createProxyServer({
-          target: { socketPath: '/tmp/webpack-dev-server.socket' },
+          target: { socketPath: ipc },
+        });
+
+        const proxyServer = http.createServer((request, response) => {
+          // You can define here your custom logic to handle the request
+          // and then proxy the request.
+          proxy.web(request, response);
+        });
+
+        proxyServer.on('upgrade', (request, socket, head) => {
+          proxy.ws(request, socket, head);
+        });
+
+        return proxyServer.listen(proxyPort, proxyHost, callback);
+      }
+
+      const proxy = await new Promise((resolve) => {
+        const proxyCreated = startProxy(() => {
+          resolve(proxyCreated);
+        });
+      });
+
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on('console', (message) => {
+          consoleMessages.push(message);
+        })
+        .on('pageerror', (error) => {
+          pageErrors.push(error);
+        });
+
+      const webSocketRequests = [];
+
+      if (webSocketServer === 'ws') {
+        const client = page._client;
+
+        client.on('Network.webSocketCreated', (test) => {
+          webSocketRequests.push(test);
+        });
+      } else {
+        page.on('request', (request) => {
+          if (/\/ws\//.test(request.url())) {
+            webSocketRequests.push({ url: request.url() });
+          }
+        });
+      }
+
+      await page.goto(`http://${proxyHost}:${proxyPort}/main`, {
+        waitUntil: 'networkidle0',
+      });
+
+      const webSocketRequest = webSocketRequests[0];
+
+      expect(webSocketRequest.url).toContain(
+        `${websocketURLProtocol}://${devServerHost}:${proxyPort}/ws`
+      );
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        'console messages'
+      );
+      expect(pageErrors).toMatchSnapshot('page errors');
+
+      proxy.close();
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+
+    it(`should work with the "ipc" option using "string" value and remove old ("${webSocketServer}")`, async () => {
+      const isWindows = process.platform === 'win32';
+      const localRelative = path.relative(process.cwd(), `${os.tmpdir()}/`);
+      const pipePrefix = isWindows ? '\\\\.\\pipe\\' : localRelative;
+      const pipeName = `webpack-dev-server.other-custom.sock`;
+      const ipc = path.join(pipePrefix, pipeName);
+
+      fs.writeFileSync(ipc, '');
+
+      const devServerHost = '127.0.0.1';
+      const proxyHost = devServerHost;
+      const proxyPort = port1;
+
+      const compiler = webpack(config);
+      const devServerOptions = {
+        webSocketServer,
+        host: devServerHost,
+        ipc,
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      await new Promise((resolve, reject) => {
+        server.listen((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      function startProxy(callback) {
+        const proxy = httpProxy.createProxyServer({
+          target: { socketPath: ipc },
         });
 
         const proxyServer = http.createServer((request, response) => {
