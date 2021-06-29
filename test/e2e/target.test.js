5ecbd77055e13f3e0c99f2c982059c740b03cfe9
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2021-06-29 20:54:58 +0300

    test: target

---
+++ test/e2e/target.test.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const webpack = require('webpack');
+const Server = require('../../lib/Server');
+const config = require('../fixtures/client-config/webpack.config');
+const runBrowser = require('../helpers/run-browser');
+const isWebpack5 = require('../helpers/isWebpack5');
+const port = require('../ports-map').target;
+
+describe('target', () => {
+  const targets = isWebpack5
+    ? [
+        false,
+        'browserslist:defaults',
+        'web',
+        'webworker',
+        'node',
+        'async-node',
+        'electron-main',
+        'electron-preload',
+        'electron-renderer',
+        'nwjs',
+        'node-webkit',
+        'es5',
+        ['web', 'es5'],
+      ]
+    : [
+        'web',
+        'webworker',
+        'node',
+        'async-node',
+        'electron-main',
+        'electron-preload',
+        'electron-renderer',
+        'node-webkit',
+      ];
+
+  for (const target of targets) {
+    it(`should work using "${target}" target`, async () => {
+      const compiler = webpack({
+        ...config,
+        target,
+        ...(target === false || target === 'es5'
+          ? {
+              output: { chunkFormat: 'array-push', path: '/' },
+            }
+          : {}),
+      });
+      const devServerOptions = {
+        host: '127.0.0.1',
+        port,
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      await new Promise((resolve, reject) => {
+        server.listen(port, '127.0.0.1', (error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on('console', (message) => {
+          consoleMessages.push(message);
+        })
+        .on('pageerror', (error) => {
+          pageErrors.push(error);
+        });
+
+      await page.goto(`http://127.0.0.1:${port}/main`, {
+        waitUntil: 'networkidle0',
+      });
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        'console messages'
+      );
+
+      if (
+        target === 'node' ||
+        target === 'async-node' ||
+        target === 'electron-main' ||
+        target === 'electron-preload' ||
+        target === 'electron-renderer' ||
+        target === 'nwjs' ||
+        target === 'node-webkit'
+      ) {
+        const hasRequireOrGlobalError =
+          pageErrors.filter((pageError) =>
+            /require is not defined|global is not defined/.test(pageError)
+          ).length === 1;
+
+        expect(hasRequireOrGlobalError).toBe(true);
+      } else {
+        expect(pageErrors).toMatchSnapshot('page errors');
+      }
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+  }
+});
