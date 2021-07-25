f4824e3d0d16847e594f7770a20f6a633c2627f1
Nitin Kumar <snitin315@gmail.com>
2021-07-25 08:10:14 +0530

    test: e2e tests for host and port options

--- test/e2e/host-and-port.test.js
+++ test/e2e/host-and-port.test.js
@@ -11,13 +11,78 @@ describe("host and port", () => {
   const hosts = ["0.0.0.0", "localhost", "127.0.0.1", "local-ip", "local-ipv4"];
 
   for (const host of hosts) {
-    it(`should work using "${host}" host`, async () => {
+    it(`should work using "${host}" host and port as number`, async () => {
       const compiler = webpack({
         ...config,
       });
       const devServerOptions = {
         host,
         port,
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      let hostname = host;
+
+      if (hostname === "local-ip" || hostname === "local-ipv4") {
+        hostname = internalIp.v4.sync();
+      }
+
+      await new Promise((resolve, reject) => {
+        server.listen(port, hostname, (error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      await page.goto(`http://${hostname}:${port}/main`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+
+    it(`should work using "${host}" host and port as string`, async () => {
+      const compiler = webpack({
+        ...config,
+      });
+      const devServerOptions = {
+        host,
+        port: `${port}`,
       };
       const server = new Server(devServerOptions, compiler);
 
