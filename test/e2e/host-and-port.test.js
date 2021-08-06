28d89dad403aed647da515d4eeba562b28a0deda
Nitin Kumar <snitin315@gmail.com>
2021-08-06 14:57:46 +0530

    test: e2e tests for host and port options (#3567)

---
+++ test/e2e/host-and-port.test.js
@@ -0,0 +1,205 @@
+"use strict";
+
+const webpack = require("webpack");
+const internalIp = require("internal-ip");
+const Server = require("../../lib/Server");
+const config = require("../fixtures/client-config/webpack.config");
+const runBrowser = require("../helpers/run-browser");
+const port = require("../ports-map")["host-and-port"];
+
+describe("host and port", () => {
+  // TODO: add local-ipv6
+  const hosts = ["0.0.0.0", "localhost", "127.0.0.1", "local-ip", "local-ipv4"];
+
+  for (const host of hosts) {
+    it(`should work using "${host}" host and port as number`, async () => {
+      const compiler = webpack(config);
+      const devServerOptions = {
+        host,
+        port,
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      let hostname = host;
+
+      if (hostname === "local-ip" || hostname === "local-ipv4") {
+        hostname = internalIp.v4.sync();
+      }
+
+      await new Promise((resolve, reject) => {
+        server.listen(port, host, (error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      await page.goto(`http://${hostname}:${port}/main`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+
+    it(`should work using "${host}" host and port as string`, async () => {
+      const compiler = webpack(config);
+      const devServerOptions = {
+        host,
+        port: `${port}`,
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      let hostname = host;
+
+      if (hostname === "local-ip" || hostname === "local-ipv4") {
+        hostname = internalIp.v4.sync();
+      }
+
+      await new Promise((resolve, reject) => {
+        server.listen(port, host, (error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      await page.goto(`http://${hostname}:${port}/main`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+
+    it(`should work using "${host}" host and "auto" port`, async () => {
+      const compiler = webpack(config);
+      const devServerOptions = {
+        host,
+        port: "auto",
+      };
+      const server = new Server(devServerOptions, compiler);
+
+      let hostname = host;
+
+      if (hostname === "local-ip" || hostname === "local-ipv4") {
+        hostname = internalIp.v4.sync();
+      }
+
+      await new Promise((resolve, reject) => {
+        server.listen("auto", host, (error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+
+      const address = server.server.address();
+      const { page, browser } = await runBrowser();
+
+      const pageErrors = [];
+      const consoleMessages = [];
+
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      await page.goto(`http://${hostname}:${address.port}/main`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+
+      await browser.close();
+      await new Promise((resolve, reject) => {
+        server.close((error) => {
+          if (error) {
+            reject(error);
+
+            return;
+          }
+
+          resolve();
+        });
+      });
+    });
+  }
+});
