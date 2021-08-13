78594da3c90f82a998f6d42a6dd11fe86fffbc72
Nitin Kumar <snitin315@gmail.com>
2021-08-13 18:31:40 +0530

    test: add e2e tests for multi compiler mode (#3612)

---
+++ test/e2e/multi-compiler.test.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const webpack = require("webpack");
+const Server = require("../../lib/Server");
+const config = require("../fixtures/multi-compiler-config/webpack.config");
+const runBrowser = require("../helpers/run-browser");
+const port = require("../ports-map")["multi-compiler"];
+
+describe("Multi compiler", () => {
+  it(`should work with multiple compilers`, async () => {
+    const compiler = webpack(config);
+    const devServerOptions = {
+      host: "127.0.0.1",
+      port,
+    };
+    const server = new Server(devServerOptions, compiler);
+
+    await server.start();
+
+    const { page, browser } = await runBrowser();
+
+    const pageErrors = [];
+    const consoleMessages = [];
+
+    page
+      .on("console", (message) => {
+        consoleMessages.push(message);
+      })
+      .on("pageerror", (error) => {
+        pageErrors.push(error);
+      });
+
+    await page.goto(`http://127.0.0.1:${port}/main`, {
+      waitUntil: "networkidle0",
+    });
+
+    expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+      "console messages"
+    );
+
+    expect(pageErrors).toMatchSnapshot("page errors");
+
+    await browser.close();
+    await server.close();
+  });
+});
