22aa35546dd8fbcbc4347ba88f3ae4b61b62115f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-14 18:51:44 +0800

    Delete examples directory

--- examples/cli/multi-compiler/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const target = document.querySelector("#target");
-
-target.classList.add("pass");
-target.innerHTML = "Success!";
-
-// This results in an error:
-// if(!window) require("test");
