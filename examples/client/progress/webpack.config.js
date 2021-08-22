005e1087d6838ac42e6d0d7d3d61c450a7dddab3
Nitin Kumar <snitin315@gmail.com>
2021-08-22 17:11:56 +0530

    docs: move `overlay` and `progress` examples to `client`

--- examples/client/progress/webpack.config.js
+++ examples/client/progress/webpack.config.js
@@ -2,10 +2,9 @@
 
 // our setup function adds behind-the-scenes bits to the config that all of our
 // examples need
-const { setup } = require("../util");
+const { setup } = require("../../util");
 
 module.exports = setup({
   context: __dirname,
-  // create error for overlay
-  entry: "./invalid.js",
+  entry: "./app.js",
 });
