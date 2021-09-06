3210ee1da9fbd22dacca7048da159465ebb78d9b
Carlo Beltrame <carlo@beltra.me>
2021-09-06 15:14:54 +0200

    fix: replace ansi-html with ansi-html-community
    This fixes the ReDoS vulnerability CVE-2021-23424

--- client-src/overlay.js
+++ client-src/overlay.js
@@ -1,7 +1,7 @@
 // The error overlay is inspired (and mostly copied) from Create React App (https://github.com/facebookincubator/create-react-app)
 // They, in turn, got inspired by webpack-hot-middleware (https://github.com/glenjamin/webpack-hot-middleware).
 
-import ansiHTML from "ansi-html";
+import ansiHTML from "ansi-html-community";
 import { encode } from "html-entities";
 
 const colors = {
