41ce5bd474f1556b3c513d01587c7c4165e08167
Nitin Kumar <snitin315@gmail.com>
2021-09-07 17:40:14 +0530

    docs: add `devMiddleware` example

---
+++ examples/dev-middleware/webpack.config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// our setup function adds behind-the-scenes bits to the config that all of our
+// examples need
+const { setup } = require("../util");
+
+module.exports = setup({
+  context: __dirname,
+  entry: "./app.js",
+  devServer: {
+    devMiddleware: {
+      index: false,
+      headers: {
+        "X-Custom-Header": "yes",
+      },
+    },
+  },
+});
