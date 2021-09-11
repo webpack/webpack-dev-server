ee34f7c66c7400aef315b2319d5ea4639ff38582
Nitin Kumar <snitin315@gmail.com>
2021-09-11 18:00:38 +0530

    docs: improve `http2` example

--- examples/http2/boolean/webpack.config.js
+++ examples/http2/boolean/webpack.config.js
@@ -2,7 +2,7 @@
 
 // our setup function adds behind-the-scenes bits to the config that all of our
 // examples need
-const { setup } = require("../util");
+const { setup } = require("../../util");
 
 module.exports = setup({
   context: __dirname,
