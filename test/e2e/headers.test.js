9cd2cd6ca7fc6cedad0947ae5ce8a9ef9aef5e4e
Nitin Kumar <snitin315@gmail.com>
2021-09-15 14:08:07 +0530

    test: add e2e tests

--- test/e2e/headers.test.js
+++ test/e2e/headers.test.js
@@ -66,6 +66,78 @@ describe("headers option", () => {
     });
   });
 
+  describe("as an array of objects", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      compiler = webpack(config);
+
+      server = new Server(
+        {
+          headers: [
+            {
+              key: "X-Foo",
+              value: "value1",
+            },
+            {
+              key: "X-Bar",
+              value: "value2",
+            },
+          ],
+          port,
+        },
+        compiler
+      );
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+    });
+
+    it("should handle GET request with headers", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`http://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(response.headers()["x-foo"]).toMatchSnapshot(
+        "response headers x-foo"
+      );
+
+      expect(response.headers()["x-bar"]).toMatchSnapshot(
+        "response headers x-bar"
+      );
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
   describe("as an array", () => {
     let compiler;
     let server;
