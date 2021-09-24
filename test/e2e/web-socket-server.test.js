5ba966a694264d795cf620344110da8468315902
evilebottnawi <sheo13666q@gmail.com>
2021-09-24 19:18:16 +0300

    fix: avoid connection when web socket server is not running

---
+++ test/e2e/web-socket-server.test.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const webpack = require("webpack");
+const Server = require("../../lib/Server");
+const config = require("../fixtures/client-config/webpack.config");
+const runBrowser = require("../helpers/run-browser");
+const port = require("../ports-map")["web-socket-server-test"];
+
+describe("web socket server", () => {
+  it("should work allow to disable", async () => {
+    const devServerPort = port;
+
+    const compiler = webpack(config);
+    const devServerOptions = {
+      webSocketServer: false,
+      port: devServerPort,
+    };
+    const server = new Server(devServerOptions, compiler);
+
+    await server.start();
+
+    const { page, browser } = await runBrowser();
+
+    const pageErrors = [];
+    const consoleMessages = [];
+
+    page
+      .on("console", (message) => {
+        consoleMessages.push(message);
+      })
+      .on("pageerror", (error) => {
+        pageErrors.push(error);
+      });
+
+    const webSocketRequests = [];
+
+    const client = page._client;
+
+    client.on("Network.webSocketCreated", (test) => {
+      webSocketRequests.push(test);
+    });
+
+    await page.goto(`http://127.0.0.1:${port}/main`, {
+      waitUntil: "networkidle0",
+    });
+
+    expect(webSocketRequests).toHaveLength(0);
+    expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+      "console messages"
+    );
+    expect(pageErrors).toMatchSnapshot("page errors");
+
+    await browser.close();
+    await server.stop();
+  });
+});
