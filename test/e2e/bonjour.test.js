263c1d5fa585633f4ab1f4becdbc9f8c1e9e08ee
Nitin Kumar <snitin315@gmail.com>
2021-11-06 08:42:48 -0700

    test: add e2e tests for `bonjour` option (#4010)

---
+++ test/e2e/bonjour.test.js
@@ -0,0 +1,459 @@
+"use strict";
+
+const os = require("os");
+const webpack = require("webpack");
+const Server = require("../../lib/Server");
+const config = require("../fixtures/simple-config/webpack.config");
+const runBrowser = require("../helpers/run-browser");
+const port = require("../ports-map").bonjour;
+
+describe("bonjour option", () => {
+  const mockPublish = jest.fn();
+  const mockUnpublishAll = jest.fn();
+
+  describe("as true", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server({ port, bonjour: true }, compiler);
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should call bonjour with correct params", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`http://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "http",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
+  describe("with 'https' option", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server({ bonjour: true, port, https: true }, compiler);
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should call bonjour with 'https' type", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`https://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "https",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
+  describe("with 'server' option", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server({ bonjour: true, port, server: "https" }, compiler);
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should call bonjour with 'https' type", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`https://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "https",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
+  describe("as object", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server(
+        {
+          port,
+          bonjour: {
+            type: "https",
+            protocol: "udp",
+          },
+        },
+        compiler
+      );
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should apply bonjour options", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`http://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "https",
+        protocol: "udp",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
+  describe("bonjour object and 'https'", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server(
+        {
+          port,
+          bonjour: {
+            type: "http",
+            protocol: "udp",
+          },
+          https: true,
+        },
+        compiler
+      );
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should apply bonjour options", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`https://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "http",
+        protocol: "udp",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+
+  describe("bonjour object and 'server' option", () => {
+    let compiler;
+    let server;
+    let page;
+    let browser;
+    let pageErrors;
+    let consoleMessages;
+
+    beforeEach(async () => {
+      jest.mock("bonjour", () => () => {
+        return {
+          publish: mockPublish,
+          unpublishAll: mockUnpublishAll,
+        };
+      });
+
+      compiler = webpack(config);
+
+      server = new Server(
+        {
+          port,
+          bonjour: {
+            type: "http",
+            protocol: "udp",
+          },
+          server: "https",
+        },
+        compiler
+      );
+
+      await server.start();
+
+      ({ page, browser } = await runBrowser());
+
+      pageErrors = [];
+      consoleMessages = [];
+    });
+
+    afterEach(async () => {
+      await browser.close();
+      await server.stop();
+      mockPublish.mockReset();
+      mockUnpublishAll.mockReset();
+    });
+
+    it("should apply bonjour options", async () => {
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const response = await page.goto(`https://127.0.0.1:${port}/main.js`, {
+        waitUntil: "networkidle0",
+      });
+
+      expect(mockPublish).toHaveBeenCalledTimes(1);
+
+      expect(mockPublish).toHaveBeenCalledWith({
+        name: `Webpack Dev Server ${os.hostname()}:${port}`,
+        port,
+        type: "http",
+        protocol: "udp",
+        subtypes: ["webpack"],
+      });
+
+      expect(mockUnpublishAll).toHaveBeenCalledTimes(0);
+
+      expect(response.status()).toMatchSnapshot("response status");
+
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+
+      expect(pageErrors).toMatchSnapshot("page errors");
+    });
+  });
+});
