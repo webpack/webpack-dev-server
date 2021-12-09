70dfd6c514b30798a17a2958a3330b2e179d3ff6
Nitin Kumar <snitin315@gmail.com>
2021-12-09 19:28:58 +0530

    chore: update `lint-staged` config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = {
-  "*.js": ["eslint --fix", "prettier --write"],
-  "*.{json,md,yml,css}": ["prettier --write"],
+  "*": ["prettier --write --ignore-unknown"],
+  "*.js": ["eslint --cache --fix"],
 };
