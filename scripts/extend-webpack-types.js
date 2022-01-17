6871ed1b7b74156ac97e5387cb4ee803b2c48d3c
Nitin Kumar <snitin315@gmail.com>
2022-01-17 17:15:29 +0530

    chore: use `graceful-fs`

--- scripts/extend-webpack-types.js
+++ scripts/extend-webpack-types.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const fs = require("fs");
 const path = require("path");
+const fs = require("graceful-fs");
 
 async function extendTypes() {
   const typesPath = path.resolve(__dirname, "../types/lib/Server.d.ts");
