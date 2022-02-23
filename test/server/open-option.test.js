0c36baae5c43a55983ad5fdf89a4699a24059108
Nitin Kumar <snitin315@gmail.com>
2022-02-23 06:16:20 +0530

    test: add case for `open` with `server` option

--- test/server/open-option.test.js
+++ test/server/open-option.test.js
@@ -63,6 +63,24 @@ describe('"open" option', () => {
     });
   });
 
+  it("should work with the 'server' option", async () => {
+    const server = new Server(
+      {
+        open: true,
+        port,
+        server: "https",
+      },
+      compiler
+    );
+
+    await server.start();
+    await server.stop();
+
+    expect(open).toHaveBeenCalledWith(`https://localhost:${port}/`, {
+      wait: false,
+    });
+  });
+
   it("should work with '0.0.0.0' host", async () => {
     const host = "0.0.0.0";
 
