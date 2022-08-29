4eed30e606cb31869dd8c00b00afdd15218eba56
chimurai <655241+chimurai@users.noreply.github.com>
2022-08-29 14:59:28 +0200

    ci: enable jest `--shard` option in github-actions (#4545)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 "use strict";
 
 module.exports = {
-  testURL: "http://localhost/",
+  testEnvironmentOptions: {
+    url: "http://localhost/",
+  },
   collectCoverage: false,
   coveragePathIgnorePatterns: [
     "/node_modules/",
@@ -12,4 +14,20 @@ module.exports = {
   snapshotResolver: "<rootDir>/test/helpers/snapshotResolver.js",
   setupFilesAfterEnv: ["<rootDir>/scripts/setupTest.js"],
   globalSetup: "<rootDir>/scripts/globalSetupTest.js",
+  moduleNameMapper: {
+    // This forces Jest/jest-environment-jsdom to use a Node+CommonJS version of uuid, not a Browser+ESM one
+    // See https://github.com/uuidjs/uuid/pull/616
+    //
+    // WARNING: if your dependency tree has multiple paths leading to uuid, this will force all of them to resolve to
+    // whichever one happens to be hoisted to your root node_modules folder. This makes it much more dangerous
+    // to consume future uuid upgrades. Consider using a custom resolver instead of moduleNameMapper.
+    //
+    // More:
+    // https://jestjs.io/docs/upgrading-to-jest28#packagejson-exports
+    // https://github.com/microsoft/accessibility-insights-web/pull/5421#issuecomment-1109168149
+    //
+    // FIXME: this uuid moduleNameMapper workaround can be removed after sockjs > uuid@v9 release
+    // https://github.com/uuidjs/uuid/pull/616#issuecomment-1206283882
+    "^uuid$": require.resolve("uuid"),
+  },
 };
