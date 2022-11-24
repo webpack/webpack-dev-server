ebe7ce8ef8f21f76bdd2469d3f8e5499e818d48d
Nitin Kumar <snitin315@gmail.com>
2022-11-24 07:39:23 +0530

    build!: minimum supported Nodejs version is 14.15.0 (#4645)
    BREAKING CHANGE: minimum supported Nodejs version is 14.15.0

--- babel.config.js
+++ babel.config.js
@@ -24,7 +24,7 @@ module.exports = (api) => {
             "@babel/preset-env",
             {
               targets: {
-                node: "12.13.0",
+                node: "14.15.0",
               },
             },
           ],
