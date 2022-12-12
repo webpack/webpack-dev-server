9b9272f5a9c306270c4ad81c03f502c89fe2bef6
Nitin Kumar <snitin315@gmail.com>
2022-12-12 17:58:25 +0530

    refactor!: remove the `content-changed` method from onSocketMessage (#4679)

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -154,20 +154,6 @@ describe("index", () => {
     expect(res).toEqual(undefined);
   });
 
-  test("should run onSocketMessage['content-changed']", () => {
-    onSocketMessage["content-changed"]();
-
-    expect(log.log.info.mock.calls[0][0]).toMatchSnapshot();
-    expect(self.location.reload).toBeCalled();
-  });
-
-  test("should run onSocketMessage['content-changed'](file)", () => {
-    onSocketMessage["content-changed"]("/public/assets/index.html");
-
-    expect(log.log.info.mock.calls[0][0]).toMatchSnapshot();
-    expect(self.location.reload).toBeCalled();
-  });
-
   test("should run onSocketMessage['static-changed']", () => {
     onSocketMessage["static-changed"]();
 
