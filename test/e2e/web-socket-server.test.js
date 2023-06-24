d67fb59c8f9f0a8beade9bdc947a69cba1830807
Nitin Kumar <snitin315@gmail.com>
2023-06-24 07:50:45 +0530

    test: refactor web socket server tests to cleanup properly

--- test/e2e/web-socket-server.test.js
+++ test/e2e/web-socket-server.test.js
@@ -21,36 +21,40 @@ describe("web socket server", () => {
 
     const { page, browser } = await runBrowser();
 
-    const pageErrors = [];
-    const consoleMessages = [];
+    try {
+      const pageErrors = [];
+      const consoleMessages = [];
 
-    page
-      .on("console", (message) => {
-        consoleMessages.push(message);
-      })
-      .on("pageerror", (error) => {
-        pageErrors.push(error);
+      page
+        .on("console", (message) => {
+          consoleMessages.push(message);
+        })
+        .on("pageerror", (error) => {
+          pageErrors.push(error);
+        });
+
+      const webSocketRequests = [];
+
+      const client = page._client;
+
+      client.on("Network.webSocketCreated", (test) => {
+        webSocketRequests.push(test);
       });
 
-    const webSocketRequests = [];
+      await page.goto(`http://127.0.0.1:${port}/`, {
+        waitUntil: "networkidle0",
+      });
 
-    const client = page._client;
-
-    client.on("Network.webSocketCreated", (test) => {
-      webSocketRequests.push(test);
-    });
-
-    await page.goto(`http://127.0.0.1:${port}/`, {
-      waitUntil: "networkidle0",
-    });
-
-    expect(webSocketRequests).toHaveLength(0);
-    expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
-      "console messages"
-    );
-    expect(pageErrors).toMatchSnapshot("page errors");
-
-    await browser.close();
-    await server.stop();
+      expect(webSocketRequests).toHaveLength(0);
+      expect(consoleMessages.map((message) => message.text())).toMatchSnapshot(
+        "console messages"
+      );
+      expect(pageErrors).toMatchSnapshot("page errors");
+    } catch (error) {
+      throw error;
+    } finally {
+      await browser.close();
+      await server.stop();
+    }
   });
 });
