da993bf1c4190461ccbabfeb4b561244d736a34d
Nitin Kumar <snitin315@gmail.com>
2023-07-31 08:14:57 +0530

    refactor: remove `file-loader` and `url-loader` deps (#4951)

--- test/fixtures/mime-types-config/webpack.config.js
+++ test/fixtures/mime-types-config/webpack.config.js
@@ -1,32 +1,12 @@
 "use strict";
 
-const { version } = require("webpack");
-
-let moduleRuleForCustom = {};
-
-if (version.startsWith("5")) {
-  moduleRuleForCustom = {
-    test: /\.custom$/,
-    type: "asset/resource",
-    generator: {
-      filename: "[name][ext]",
-    },
-  };
-} else {
-  moduleRuleForCustom = {
-    test: /\.custom$/,
-    use: [
-      {
-        loader: "file-loader",
-        options: {
-          name() {
-            return "[name].[ext]";
-          },
-        },
-      },
-    ],
-  };
-}
+const moduleRuleForCustom = {
+  test: /\.custom$/,
+  type: "asset/resource",
+  generator: {
+    filename: "[name][ext]",
+  },
+};
 
 module.exports = {
   mode: "development",
