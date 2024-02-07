3b1341a3a0dbadbf61f2704ebd0458e5158f9c5d
alexander.akait <sheo13666q@gmail.com>
2024-02-07 15:10:57 +0300

    test: debug

--- scripts/setupTest.js
+++ scripts/setupTest.js
@@ -1,10 +1,5 @@
 "use strict";
 
-if (process.platform === "win32") {
-  // eslint-disable-next-line no-console
-  console.log("");
-}
-
 process.env.CHOKIDAR_USEPOLLING = true;
 
 jest.setTimeout(300000);
