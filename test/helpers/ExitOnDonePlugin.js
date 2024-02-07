a9c0e5d657b544539142c65ce2374f0a88d76220
alexander.akait <sheo13666q@gmail.com>
2024-02-07 20:25:43 +0300

    test: debug

--- test/helpers/ExitOnDonePlugin.js
+++ test/helpers/ExitOnDonePlugin.js
@@ -3,12 +3,6 @@
 module.exports = class ExitOnDonePlugin {
   // eslint-disable-next-line class-methods-use-this
   apply(compiler) {
-    let done = false;
-
-    compiler.options.infrastructureLogging.stream.on("error", () => {
-      done = true;
-    });
-
     compiler.hooks.afterDone.tap("webpack-dev-server", (stats) => {
       let exitCode = 0;
 
@@ -16,17 +10,8 @@ module.exports = class ExitOnDonePlugin {
         exitCode = 1;
       }
 
-      process.nextTick(() => {
-        compiler.options.infrastructureLogging.stream.destroy(
-          new Error("done"),
-        );
-
-        const interval = setInterval(() => {
-          if (done) {
-            clearInterval(interval);
-            process.exit(exitCode);
-          }
-        }, 100);
+      setImmediate(() => {
+        process.exit(exitCode);
       });
     });
   }
