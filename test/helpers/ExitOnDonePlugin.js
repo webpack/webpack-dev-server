76958bf3eab82b5804f5de6baf17b7297e725605
alexander.akait <sheo13666q@gmail.com>
2024-02-07 20:30:12 +0300

    test: debug

--- test/helpers/ExitOnDonePlugin.js
+++ test/helpers/ExitOnDonePlugin.js
@@ -10,7 +10,40 @@ module.exports = class ExitOnDonePlugin {
         exitCode = 1;
       }
 
-      setImmediate(() => {
+      const streams = [process.stderr];
+
+      let drainCount = 0;
+
+      // Actually exit if all streams are drained.
+      function tryToExit() {
+        if (drainCount === streams.length) {
+          process.exit(exitCode);
+        }
+      }
+
+      streams.forEach((stream) => {
+        // Count drained streams now, but monitor non-drained streams.
+        if (stream.bufferSize === 0) {
+          drainCount += 1;
+        } else {
+          stream.write("", "utf-8", () => {
+            drainCount += 1;
+
+            tryToExit();
+          });
+        }
+
+        // Prevent further writing.
+        stream.write = () => {};
+      });
+
+      // If all streams were already drained, exit now.
+      tryToExit();
+
+      // In Windows, when run as a Node.js child process, a script utilizing
+      // this library might just exit with a 0 exit code, regardless. This code,
+      // despite the fact that it looks a bit crazy, appears to fix that.
+      process.on("exit", () => {
         process.exit(exitCode);
       });
     });
