726fba745601275bd0d0885fc16c5a25a43e0021
alexander.akait <sheo13666q@gmail.com>
2024-02-07 17:58:23 +0300

    test: debug

--- test/helpers/ExitOnDonePlugin.js
+++ test/helpers/ExitOnDonePlugin.js
@@ -3,7 +3,7 @@
 module.exports = class ExitOnDonePlugin {
   // eslint-disable-next-line class-methods-use-this
   apply(compiler) {
-    compiler.hooks.done.tap("webpack-dev-server", (stats) => {
+    compiler.hooks.afterDone.tap("webpack-dev-server", (stats) => {
       let exitCode = 0;
 
       if (stats.hasErrors()) {
