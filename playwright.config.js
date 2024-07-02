b24cac011a23689d06c83859b94002352295fc5a
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-07-02 13:33:58 +0200

    chore: update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,17 +1,12 @@
 "use strict";
 
-const CI = false;
-
 module.exports = {
   testDir: "./test/e2e-playwright",
-  // however this can have benefits, in tests I couldn't start a server on multiple ports
-  // for tests "stats-refactored.test.js" the port is read from "ports-map.js" file somehow
-  // I should manage to handle it if I want to run the tests in parallel mode
   fullyParallel: false,
-  forbidOnly: !!CI,
-  retries: CI ? 2 : 0,
+  forbidOnly: process.env.CI !== "true",
+  retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: "html",
+  reporter: process.env.CI ? "github" : "list",
   use: {
     trace: "on-first-retry",
   },
