5cd56588f2c86ca1792645f8c5e7a9e17718409d
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-07-19 14:34:33 +0200

    feat: update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,12 @@
 "use strict";
 
 module.exports = {
+  globalSetup: require.resolve("./scripts/setupPlaywright.js"),
   testIgnore: "**/*.ignore.*",
   testDir: "./test/e2e",
-  outputDir: ".test/spec/output",
-  snapshotPathTemplate:
-    ".test/spec/snaps/{projectName}/{testFilePath}/{arg}{ext}",
   fullyParallel: false,
-  forbidOnly: process.env.CI !== "true",
+  forbidOnly: !!process.env.CI,
+  // TODO: can help with flakiness, make sure it works on CI
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   reporter: process.env.CI ? "github" : "list",
