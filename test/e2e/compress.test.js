ea20ee77002b5710449a384f8f8de12fe14f6e05
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-07-24 14:58:01 +0200

    feat: update labels, add a flaky and a mistake in snapshot

--- test/e2e/compress.test.js
+++ test/e2e/compress.test.js
@@ -7,7 +7,7 @@ const { expect } = require("../helpers/playwright-custom-expects");
 const config = require("../fixtures/simple-config-other/webpack.config");
 const port = require("../ports-map")["compress-option"];
 
-describe("compress option", { tag: "@flaky" }, () => {
+describe("compress option", { tag: ["@flaky", "@fails"] }, () => {
   describe("enabled by default when not specified", () => {
     let compiler;
     let server;
@@ -44,13 +44,11 @@ describe("compress option", { tag: "@flaky" }, () => {
 
       expect(response.status()).toMatchSnapshotWithArray();
 
-      expect(
-        response.headers()["content-encoding"])
-      .toMatchSnapshotWithArray();
+      expect(response.headers()["content-encoding"]).toMatchSnapshotWithArray();
 
       expect(
-        consoleMessages.map((message) => message.text()))
-      .toMatchSnapshotWithArray();
+        consoleMessages.map((message) => message.text()),
+      ).toMatchSnapshotWithArray();
 
       expect(pageErrors).toMatchSnapshotWithArray();
     });
@@ -98,13 +96,11 @@ describe("compress option", { tag: "@flaky" }, () => {
 
       expect(response.status()).toMatchSnapshotWithArray();
 
-      expect(
-        response.headers()["content-encoding"])
-      .toMatchSnapshotWithArray();
+      expect(response.headers()["content-encoding"]).toMatchSnapshotWithArray();
 
       expect(
-        consoleMessages.map((message) => message.text()))
-      .toMatchSnapshotWithArray();
+        consoleMessages.map((message) => message.text()),
+      ).toMatchSnapshotWithArray();
 
       expect(pageErrors).toMatchSnapshotWithArray();
     });
@@ -137,35 +133,44 @@ describe("compress option", { tag: "@flaky" }, () => {
       await server.stop();
     });
 
-    test("should handle GET request to bundle file", {
-      annotation: {
-        type: "fails",
-        description: "https://github.com/webpack/webpack-dev-server/issues/4630#issuecomment-1588211112"
-      }
-    }, async ({ page }) => {
-      page
-        .on("console", (message) => {
-          consoleMessages.push(message);
-        })
-        .on("pageerror", (error) => {
-          pageErrors.push(error);
+    test(
+      "should handle GET request to bundle file",
+      {
+        tag: ["@flaky"],
+        annotation: {
+          type: "issue",
+          description:
+            "https://github.com/webpack/webpack-dev-server/blob/master/test/e2e/__snapshots__/compress.test.js.snap.webpack5#L7",
+        },
+      },
+      async ({ page }) => {
+        page
+          .on("console", (message) => {
+            consoleMessages.push(message);
+          })
+          .on("pageerror", (error) => {
+            pageErrors.push(error);
+          });
+
+        const response = await page.goto(`http://127.0.0.1:${port}/main.js`, {
+          waitUntil: "networkidle0",
         });
 
-      const response = await page.goto(`http://127.0.0.1:${port}/main.js`, {
-        waitUntil: "networkidle0",
-      });
+        expect(response.status()).toMatchSnapshotWithArray();
 
-      expect(response.status()).toMatchSnapshotWithArray();
+        // the response sometimes is []
+        // and sometimes {"accept-ranges": "bytes", "connection": "keep-alive", "content-length": "276518", "content-type": "application/javascript; charset=utf-8", "date": "Wed, 24 Jul 2024 12:49:54 GMT", "keep-alive": "timeout=5", "x-powered-by": "Express"}
+        // the thing is that the content-encoding does not exist in the response headers object
+        expect(
+          response.headers()["content-encoding"],
+        ).toMatchSnapshotWithArray();
 
-      expect(
-        response.headers()["content-encoding"])
-      .toMatchSnapshotWithArray();
+        expect(
+          consoleMessages.map((message) => message.text()),
+        ).toMatchSnapshotWithArray();
 
-      expect(
-        consoleMessages.map((message) => message.text()))
-      .toMatchSnapshotWithArray();
-
-      expect(pageErrors).toMatchSnapshotWithArray();
-    });
+        expect(pageErrors).toMatchSnapshotWithArray();
+      },
+    );
   });
 });
