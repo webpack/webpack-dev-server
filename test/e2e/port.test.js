73c7b04e345c7a4273d6dde4d1572afae16d5e03
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-07-24 15:16:29 +0200

    fix: lint error

--- test/e2e/port.test.js
+++ test/e2e/port.test.js
@@ -7,7 +7,7 @@ const { expect } = require("../helpers/playwright-custom-expects");
 const config = require("../fixtures/client-config/webpack.config");
 const port = require("../ports-map").port;
 
-// FIXME: duplicate port, should check on pupetter and with the team
+// FIXME: duplicate port, should check on puppeteer and with the team
 describe("port", () => {
   const ports = [
     "<not-specified>",
