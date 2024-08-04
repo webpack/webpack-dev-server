790a871ea87355b87420586853663191cbcf2dcc
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-08-04 17:43:08 +0200

    fix: code coverage

---
+++ nyc.config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  include: ["lib/**"],
+  reporter: ["html", "text", "lcov"],
+  excludeNodeModules: true,
+};
--- test/helpers/playwright-test.js
+++ test/helpers/playwright-test.js
@@ -1,15 +1,6 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
-const crypto = require("crypto");
 const { test } = require("@playwright/test");
-
-const istanbulCLIOutput = path.join(process.cwd(), ".nyc_output");
-
-function generateUUID() {
-  return crypto.randomBytes(16).toString("hex");
-}
 
 const customTest = test.extend({
   done: [
@@ -26,35 +17,6 @@ const customTest = test.extend({
     },
     { option: true },
   ],
-  context: async ({ context }, use) => {
-    await context.addInitScript(() =>
-      // eslint-disable-next-line no-undef
-      window.addEventListener("beforeunload", () =>
-        // eslint-disable-next-line no-undef
-        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__)),
-      ),
-    );
-    await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
-    await context.exposeFunction("collectIstanbulCoverage", (coverageJSON) => {
-      if (coverageJSON) {
-        fs.writeFileSync(
-          path.join(
-            istanbulCLIOutput,
-            `playwright_coverage_${generateUUID()}.json`,
-          ),
-          coverageJSON,
-        );
-      }
-    });
-    await use(context);
-    for (const page of context.pages()) {
-      // eslint-disable-next-line no-await-in-loop
-      await page.evaluate(() =>
-        // eslint-disable-next-line no-undef
-        window.collectIstanbulCoverage(JSON.stringify(window.__coverage__)),
-      );
-    }
-  },
 });
 
 module.exports = { test: customTest };
