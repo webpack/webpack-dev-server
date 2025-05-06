f86898cb2b2f66614df48cdba47a97d32750f70b
Emervin <208295705+Vinnn021414@users.noreply.github.com>
2025-05-06 10:28:42 +0800

    Delete examples directory

--- examples/general/config-promise/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Change the following line and save to see the compilation status
-
-const target = document.querySelector("#target");
-
-target.classList.add("pass");
-target.innerHTML = "Success!";
--- examples/general/universal-config/server.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-console.log("webpack-dev-server/server");
