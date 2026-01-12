54976c95faeec3d2219628b85fd3a4d681b1844c
Sebastian Beltran <bjohansebas@gmail.com>
2026-01-12 20:02:10 +0000

    chore: remove SockJS support and related tests from the project

--- test/e2e/hot-and-live-reload.test.js
+++ test/e2e/hot-and-live-reload.test.js
@@ -6,7 +6,6 @@
 
 const path = require("node:path");
 const fs = require("graceful-fs");
-const SockJS = require("sockjs-client");
 const webpack = require("webpack");
 const WebSocket = require("ws");
 const Server = require("../../lib/Server");
@@ -25,7 +24,6 @@ const cssFilePath = path.resolve(
 const INVALID_MESSAGE = "[webpack-dev-server] App updated. Recompiling...";
 
 describe("hot and live reload", () => {
-  // "sockjs" client cannot add additional headers
   const modes = [
     {
       title: "should work and refresh content using hot module replacement",
@@ -129,70 +127,6 @@ describe("hot and live reload", () => {
         "should work and refresh content using hot module replacement when live reload and hot enabled",
       options: {
         webSocketServer: "ws",
-        liveReload: true,
-        hot: true,
-      },
-    },
-    // "sockjs" web socket serve
-    {
-      title:
-        "should work and refresh content using hot module replacement when hot enabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
-        hot: true,
-      },
-    },
-    {
-      title:
-        "should work and refresh content using hot module replacement when live reload enabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
-        liveReload: true,
-      },
-    },
-    {
-      title: "should not refresh content when hot and no live reload disabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
-        hot: false,
-        liveReload: false,
-      },
-    },
-    {
-      title:
-        "should work and refresh content using hot module replacement when live reload disabled and hot enabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
-        liveReload: false,
-        hot: true,
-      },
-    },
-    {
-      title:
-        "should work and refresh content using live reload when live reload disabled and hot enabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
-        liveReload: true,
-        hot: false,
-      },
-    },
-    {
-      title:
-        "should work and refresh content using hot module replacement when live reload and hot enabled",
-      options: {
-        allowedHosts: "all",
-
-        webSocketServer: "sockjs",
         liveReload: true,
         hot: true,
       },
@@ -347,89 +281,44 @@ describe("hot and live reload", () => {
         testDevServerOptions.webSocketServer !== false;
 
       await new Promise((resolve) => {
-        const webSocketTransport =
-          typeof testDevServerOptions.webSocketServer !== "undefined" &&
-          testDevServerOptions.webSocketServer !== false
-            ? testDevServerOptions.webSocketServer
-            : "ws";
+        const ws = new WebSocket(`ws://localhost:${devServerOptions.port}/ws`, {
+          headers: {
+            host: `localhost:${devServerOptions.port}`,
+            origin: `http://localhost:${devServerOptions.port}`,
+          },
+        });
 
-        if (webSocketTransport === "ws") {
-          const ws = new WebSocket(
-            `ws://localhost:${devServerOptions.port}/ws`,
-            {
-              headers: {
-                host: `localhost:${devServerOptions.port}`,
-                origin: `http://localhost:${devServerOptions.port}`,
-              },
-            },
-          );
+        let opened = false;
+        let received = false;
+        let errored = false;
 
-          let opened = false;
-          let received = false;
-          let errored = false;
+        ws.on("error", (_error) => {
+          errored = true;
 
-          ws.on("error", (_error) => {
-            errored = true;
+          ws.close();
+        });
+
+        ws.on("open", () => {
+          opened = true;
+        });
+
+        ws.on("message", (data) => {
+          const message = JSON.parse(data);
+
+          if (message.type === "ok") {
+            received = true;
 
             ws.close();
-          });
+          }
+        });
 
-          ws.on("open", () => {
-            opened = true;
-          });
-
-          ws.on("message", (data) => {
-            const message = JSON.parse(data);
-
-            if (message.type === "ok") {
-              received = true;
-
-              ws.close();
-            }
-          });
-
-          ws.on("close", () => {
-            if (opened && received && !errored) {
-              resolve();
-            } else if (!webSocketServerLaunched && errored) {
-              resolve();
-            }
-          });
-        } else {
-          const sockjs = new SockJS(
-            `http://localhost:${devServerOptions.port}/ws`,
-          );
-
-          let opened = false;
-          let received = false;
-          let errored = false;
-
-          sockjs.onerror = () => {
-            errored = true;
-          };
-
-          sockjs.onopen = () => {
-            opened = true;
-          };
-
-          sockjs.onmessage = ({ data }) => {
-            const message = JSON.parse(data);
-
-            if (message.type === "ok") {
-              received = true;
-
-              sockjs.close();
-            }
-          };
-
-          sockjs.onclose = (event) => {
-            if (opened && received && !errored) {
-              resolve();
-            } else if (event && event.reason === "Cannot connect to server") {
-              resolve();
-            }
-          };
-        }
+        ws.on("close", () => {
+          if (opened && received && !errored) {
+            resolve();
+          } else if (!webSocketServerLaunched && errored) {
+            resolve();
+          }
+        });
       });
 
       const launched = await runBrowser();
