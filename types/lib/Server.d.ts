e61dfcbd67fe2ed72b6e20f5fa464e416a17373d
Sebastian Beltran <bjohansebas@gmail.com>
2026-02-01 10:12:14 -0500

    feat: use new URL instead of url.parse (#5637)

--- types/lib/Server.d.ts
+++ types/lib/Server.d.ts
@@ -1415,6 +1415,7 @@ declare class Server<
    * @param {((err?: Error) => void)=} callback callback
    */
   stopCallback(callback?: ((err?: Error) => void) | undefined): void;
+  #private;
 }
 declare namespace Server {
   export {
