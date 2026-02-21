2a31a3581af017f8161899786f39fe0a50ff7f3d
Sebastian Beltran <bjohansebas@gmail.com>
2026-02-21 18:12:12 +0000

    fix: correct errors when a compiler is not passed to the constructor

---
+++ test/helpers/compile.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = (compiler) =>
+  new Promise((resolve, reject) => {
+    compiler.run((error, stats) => {
+      if (error) {
+        return reject(error);
+      }
+
+      return resolve(stats);
+    });
+  });
